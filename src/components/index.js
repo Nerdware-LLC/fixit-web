4164d4ea610eb91e568d065a4ddc5421fc1a2256
trevor-anderson <T.AndersonProperty@gmail.com>
2022-03-23 22:00:57 -0400

    refactor: update named component exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,14 @@
+export { Button } from "./Button";
 export { Dialog, useDialog } from "./Dialog";
+export { ErrorBoundary } from "./ErrorBoundary";
 export {
   Loading,
   Error,
   FetchStateContextWrapper,
   useFetchStateContext
 } from "./Indicators";
+export { StripeBadge } from "./StripeBadge";
 export { StripeForm } from "./StripeForm";
 export { SlideTransition } from "./Transitions";
-export { Caption, ErrorMessage, Text, Title } from "./Typography";
-export {
-  WebViewContentContainer,
-  WebViewContextProvider,
-  WebViewPageContainer,
-  useWebViewContext
-} from "./WebView";
-export { Button } from "./Button";
-export { ErrorBoundary } from "./ErrorBoundary";
-export { NotFound } from "./NotFound";
-export { StripeBadge } from "./StripeBadge";
+export { Text, Title, ErrorMessage } from "./Typography";
+export { WebViewContainer, useWebViewContext } from "./WebView";
