936ed47b9e5ddb20c1ff24df06920a0b316bb35f
trevor-anderson <T.AndersonProperty@gmail.com>
2022-03-23 21:27:38 -0400

    feat: add storage mock and prefix token key w underscore

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,8 +1,8 @@
+const _AUTH_TOKEN_KEY = "@authToken";
+
 export const storage = {
   // AUTH TOKEN
-  setAuthToken: str => localStorage.setItem(AUTH_TOKEN_KEY, str),
-  getAuthToken: () => localStorage.getItem(AUTH_TOKEN_KEY),
-  removeAuthToken: () => localStorage.removeItem(AUTH_TOKEN_KEY)
+  setAuthToken: str => localStorage.setItem(_AUTH_TOKEN_KEY, str),
+  getAuthToken: () => localStorage.getItem(_AUTH_TOKEN_KEY),
+  removeAuthToken: () => localStorage.removeItem(_AUTH_TOKEN_KEY)
 };
-
-const AUTH_TOKEN_KEY = "@authToken";
