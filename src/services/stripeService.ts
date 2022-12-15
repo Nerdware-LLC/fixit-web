cc151f344095772ebaac9095275b35438b3097c2
trevor-anderson <T.AndersonProperty@gmail.com>
2022-12-15 10:17:17 -0500

    docs: add some jsdoc to stripeService obj

--- src/services/stripeService.ts
+++ src/services/stripeService.ts
@@ -1,7 +1,21 @@
 import { httpService } from "./httpService";
-import { logger } from "../utils";
-import type { EncodedAuthToken } from "../types";
+import type { EncodedAuthToken } from "@types";
 
+/**
+ * Fixit API Stripe-Service Methods
+ *
+ * - `submitPaymentForSubscription` — // TODO add jsdoc for stripeService.submitPaymentForSubscription
+ *
+ * - `getCustomerPortalLink` — This method obtains a link to the Stripe-provided
+ *   customer portal page. Once finished, customers are returned right to where
+ *   they left off (`window.location.href`).
+ *
+ * - `getConnectOnboardingLink` — This method obtains a link to the Stripe-provided
+ *   Connect onboarding page. Once finished, customers are returned right to where
+ *   they left off (`window.location.href`).
+ *
+ * - `getConnectDashboardLink` — // TODO add jsdoc for stripeService.getConnectDashboardLink
+ */
 export const stripeService = {
   submitPaymentForSubscription: async ({
     selectedSubscription,
@@ -12,27 +26,27 @@ export const stripeService = {
     paymentMethodID: string;
     promoCode?: string;
   }): Promise<{ token: EncodedAuthToken }> => {
-    logger.stripe("submitting payment for subscription...");
     return await httpService.post("/api/subscriptions/submit-payment", {
       selectedSubscription,
       promoCode,
       paymentMethodID
     });
   },
-  getCustomerPortalLink: async (): Promise<{ stripeLink: string }> => {
-    logger.stripe("fetching customer portal link...");
-    return await httpService.get("/api/subscriptions/customer-portal");
+  getCustomerPortalLink: async (): Promise<StripeServiceLinkResponse> => {
+    return await httpService.post("/api/subscriptions/customer-portal", {
+      returnURL: window.location.href
+    });
   },
-  getConnectOnboardingLink: async (): Promise<{ stripeLink: string }> => {
-    logger.stripe("fetching connect onboarding account link...");
-    return await httpService.get("/api/connect/account-link");
+  getConnectOnboardingLink: async (): Promise<StripeServiceLinkResponse> => {
+    return await httpService.post("/api/connect/account-link", {
+      returnURL: window.location.href
+    });
   },
-  getConnectDashboardLink: async (): Promise<{ stripeLink: string }> => {
-    logger.stripe("fetching connect dashboard link...");
+  getConnectDashboardLink: async (): Promise<StripeServiceLinkResponse> => {
     return await httpService.get("/api/connect/dashboard-link");
   }
 };
 
-export type StripeServiceLinkResponse = {
+type StripeServiceLinkResponse = {
   stripeLink: string;
 };
