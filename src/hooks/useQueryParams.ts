8116c7f4e712173eae5a02f37e8e7e0f3febac26
trevor-anderson <T.AndersonProperty@gmail.com>
2022-12-15 09:05:04 -0500

    feat: rm useQueryParams hook

--- src/hooks/useQueryParams.ts
+++
@@ -1,7 +0,0 @@
-import { useLocation } from "react-router-dom";
-
-export const useQueryParams = () => {
-  const urlSearchParams = new URLSearchParams(useLocation().search);
-
-  return Object.fromEntries(urlSearchParams.entries());
-};
