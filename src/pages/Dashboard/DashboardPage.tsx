049036ba0b048f57fa66189c5a37962a95f748f3
trevor-anderson <T.AndersonProperty@gmail.com>
2022-12-15 10:49:18 -0500

    feat: add pages/Dashboard

---
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -0,0 +1,15 @@
+export const DashboardPage = () => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <h1>Dashboard Page</h1>
+    </div>
+  );
+};
