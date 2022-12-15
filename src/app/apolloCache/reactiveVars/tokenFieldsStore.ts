b3d307d3028ba89812312843fa1075b211dadcfc
trevor-anderson <T.AndersonProperty@gmail.com>
2022-12-15 09:04:17 -0500

    feat: rm tokenFieldsStore

--- src/app/apolloCache/reactiveVars/tokenFieldsStore.ts
+++
@@ -1,4 +0,0 @@
-import { ReactiveStore } from "./ReactiveStore";
-import type { AuthTokenPayload } from "../../../types";
-
-export const tokenFieldsStore = new ReactiveStore<AuthTokenPayload | undefined>();
