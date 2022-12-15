e06304878bcdff35ffd283fd03fe8cdf4eca688c
trevor-anderson <T.AndersonProperty@gmail.com>
2022-12-15 10:10:05 -0500

    docs: clarify commentary in setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,4 @@
-// jest-dom adds custom jest matchers for test assertions on DOM nodes:
-// expect(element).toHaveTextContent(/react/i)
+// This file corresponds to Jest config "setupFilesAfterEnv"
+// react-testing-library renders components to document.body
+// jest-dom adds custom jest matchers for test assertions on DOM nodes
 import "@testing-library/jest-dom";
