5c7315a9d48772302d353786b46b8706089e0c6c
trevor-anderson <T.AndersonProperty@gmail.com>
2022-12-15 10:50:09 -0500

    feat: add pages/ProfilePage

---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,15 @@
+export const ProfilePage = () => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <h1>Profile Page</h1>
+    </div>
+  );
+};
