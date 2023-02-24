63579951ff3c494b813b573c6fc113f19fd14d2f
trevor-anderson <T.AndersonProperty@gmail.com>
2023-02-24 08:40:07 -0500

    refactor: rm Mui x- comp theme aug imports (mv'd to themes file)

--- src/app/LocalizationProvider.tsx
+++ src/app/LocalizationProvider.tsx
@@ -1,13 +1,9 @@
 import { LocalizationProvider as MuiLocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
-import type {} from "@mui/x-date-pickers/themeAugmentation"; // see jsdoc
 
 /**
  * This component provides dateTime localization functionality, and
  * is necessary to run the DateTimePicker components.
- *
- * - Why the empty type import from @mui/x-date-pickers? See link below.
- *   https://mui.com/x/react-date-pickers/getting-started/#typescript
  */
 export const LocalizationProvider = ({ children }: { children: React.ReactNode }) => (
   <MuiLocalizationProvider dateAdapter={AdapterMoment}>{children}</MuiLocalizationProvider>
