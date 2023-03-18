dac6eebff89108c0eb54e0915b5f2b6076430ffe
trevor-anderson <T.AndersonProperty@gmail.com>
2023-03-18 06:14:25 -0400

    feat: rm old 'ListSettingsReactiveStore' type

--- src/types/propTypes/reactiveStoreType.ts
+++ src/types/propTypes/reactiveStoreType.ts
@@ -1,6 +1,4 @@
 import { instanceOf } from "prop-types";
 import { ReactiveStore } from "@cache/reactiveVars/ReactiveStore";
-import { ListSettingsStore } from "@cache/reactiveVars/ListSettingsStore";
 
 export const reactiveStoreType = instanceOf(ReactiveStore);
-export const listSettingsStoreType = instanceOf(ListSettingsStore);
