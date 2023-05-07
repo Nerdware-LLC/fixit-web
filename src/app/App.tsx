86439af31e6f1571cc9458c96d35b5b3b66c7ee9
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 09:40:59 -0400

    style(prettier): implement new prettier rules

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import { ApolloProvider } from "@apollo/client/react/context";
-import { apolloClient } from "./apolloClient";
+import { ErrorBoundary } from "@components/ErrorBoundary";
+import { RootAppRouter } from "@routers/RootAppRouter";
+import { DateTimeLocalizationProvider } from "./DateTimeLocalizationProvider";
+import { GlobalStyles } from "./GlobalStyles";
 import { PageLayoutContextProvider } from "./PageLayoutContext";
-import { WebViewContextProvider } from "./WebViewContext";
 import { ThemeProvider } from "./ThemeProvider";
-import { LocalizationProvider } from "./LocalizationProvider";
-import { GlobalStyle } from "./GlobalStyle";
 import { ToastContainer } from "./ToastContainer";
-import { RootAppRouter } from "@navigation";
-import { ErrorBoundary } from "@components";
+import { WebViewContextProvider } from "./WebViewContext";
+import { apolloClient } from "./apolloClient";
 
 export const App = () => (
   <ApolloProvider client={apolloClient}>
@@ -15,11 +15,11 @@ export const App = () => (
       <PageLayoutContextProvider>
         <WebViewContextProvider>
           <ThemeProvider>
-            <LocalizationProvider>
-              <GlobalStyle />
+            <DateTimeLocalizationProvider>
+              <GlobalStyles />
               <RootAppRouter />
               <ToastContainer />
-            </LocalizationProvider>
+            </DateTimeLocalizationProvider>
           </ThemeProvider>
         </WebViewContextProvider>
       </PageLayoutContextProvider>
