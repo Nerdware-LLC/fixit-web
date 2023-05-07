130b76f215aa91a7f2984fe9b18c23f393cdf351
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 08:48:52 -0400

    feat(locale): add dayjs localization imports

---
+++ src/app/DateTimeLocalizationProvider.tsx
@@ -0,0 +1,32 @@
+import "dayjs/locale/en";
+import dayjs from "dayjs";
+import advancedFormatDayJsPlugin from "dayjs/plugin/advancedFormat";
+import isBetweenDayJsPlugin from "dayjs/plugin/isBetween";
+import localizedFormatDayJsPlugin from "dayjs/plugin/localizedFormat";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import {
+  LocalizationProvider as MuiLocalizationProvider,
+  type LocalizationProviderProps,
+} from "@mui/x-date-pickers/LocalizationProvider";
+
+/**
+ * This component provides date/time localization functionality using the Mui
+ * `LocalizationProvider` with the `dayjs` adapter. This component is
+ * necessary to run the Mui DatePicker/DateTimePicker components.
+ *
+ * > Currently, only the `en` locale is supported.
+ */
+export const DateTimeLocalizationProvider = ({
+  children,
+  ...props
+}: LocalizationProviderProps<unknown>) => (
+  <MuiLocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en" {...props}>
+    {children}
+  </MuiLocalizationProvider>
+);
+
+// DayJS Plugins:
+
+dayjs.extend(advancedFormatDayJsPlugin); // provides 1-24 hour format used by logger util
+dayjs.extend(isBetweenDayJsPlugin); // provides isBetween() method used by Dashboard itemDataParsers
+dayjs.extend(localizedFormatDayJsPlugin); // provides local date format used in CheckoutPage
