3a9016b51a7881fca855f2a9707f3522a3b5bfcb
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 09:48:22 -0400

    refactor: rm exports of deleted files

--- src/app/apolloCache/typePolicies/index.ts
+++ src/app/apolloCache/typePolicies/index.ts
@@ -1,13 +1,1 @@
-import { invoiceTypePolicy } from "./invoiceTypePolicy";
-import { profileTypePolicy } from "./profileTypePolicy";
-import { queryTypePolicy } from "./queryTypePolicy";
-import { userTypePolicy } from "./userTypePolicy";
-import { woTypePolicy } from "./woTypePolicy";
-
-export const typePolicies = {
-  ...invoiceTypePolicy,
-  ...profileTypePolicy,
-  ...queryTypePolicy,
-  ...userTypePolicy,
-  ...woTypePolicy
-};
+export * from "./Query";
