35171d8bfc963f6b4ebbfce266f2990a94334fda
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 09:17:58 -0400

    perf: rm index file to enforce specific imports for tree-shaking

--- src/app/apolloCache/reactiveVars/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./checkoutValuesStore";
-export * from "./isActiveAccountStore";
-export * from "./isAuthenticatedStore";
-export * from "./isConnectOnboardingNeededStore";
-export * from "./themeStore";
