4a81b750db63ccffdf9d15a5f9a718f3ffda07ff
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 10:20:37 -0400

    style(prettier): implement new prettier rules

--- src/hooks/useApiService.ts
+++ src/hooks/useApiService.ts
@@ -1,54 +1,37 @@
-import { useFetchStateContext } from "@components";
-import { getTypeSafeErr } from "@utils";
-import { useAuthToken } from "./useAuthToken";
-import { useProcessPreFetchedUserItems } from "./useProcessPreFetchedUserItems";
-import type { AuthTokenPayload } from "@types";
-import type { AuthServiceToken, PreFetchedUserItems, StripeServiceLinkResponse } from "@services";
+import { authenticatedUserStore } from "@cache/authenticatedUserStore";
+import { useFetchStateContext } from "@components/Indicators/useFetchStateContext";
+import { getTypeSafeErr } from "@utils/typeSafety";
+import type { AuthServiceToken } from "@services/authService";
 
 /**
  * This hook is used by API-service-related hooks like `useAuthService` and
  * `useStripeService` to provide their methods with common fetch-related logic
  * handling, like triggering loading/error states.
- *
- * For convenience, this hook also provides apiResponseHandler methods via
- * the `checkApiResponse` object; these methods can be called independently,
- * OR provided within an array for the 2nd arg of `handleApiServiceRequest`
- * ("apiResponseHandlers").
  */
 export const useApiService = () => {
   const { setIsLoading, setError } = useFetchStateContext();
-  const { processAuthToken, removeAuthToken } = useAuthToken();
-  const { processPreFetchedUserItems } = useProcessPreFetchedUserItems();
 
-  const checkApiResponse = {
-    forAuthToken: async (apiResponse?: Partial<AuthServiceToken>) => {
-      return {
-        success: !!apiResponse?.token,
-        ...(typeof apiResponse?.token === "string" && {
-          token: await processAuthToken(apiResponse.token)
-        })
-      };
-    },
-    forPrefetchedUserItems: async (
-      apiResponse?: { token?: AuthTokenPayload } & Partial<PreFetchedUserItems>
-    ) => {
-      if (typeof apiResponse?.token?.id === "string" && !!apiResponse?.userItems) {
-        await processPreFetchedUserItems(apiResponse.token.id, apiResponse.userItems);
-      }
-    },
-    forStripeLink: (apiResponse?: Partial<StripeServiceLinkResponse>) => {
-      if (typeof apiResponse?.stripeLink === "string") {
-        window.open(apiResponse.stripeLink);
-      }
-    }
+  const checkApiResponseForAuthToken = (apiResponse?: Partial<AuthServiceToken>) => {
+    return {
+      success: !!apiResponse?.token,
+      ...(typeof apiResponse?.token === "string" && {
+        token: authenticatedUserStore.processAuthToken(apiResponse.token),
+      }),
+    };
   };
 
   const handleApiServiceRequest = async (
     apiServiceMethod: (...args: any[]) => Promise<any>,
-    apiResponseHandlers?: Array<(apiResponse?: any) => any | Promise<any>>
+    {
+      shouldShowRequestLoadingIndicator = true,
+      apiResponseHandlers,
+    }: {
+      shouldShowRequestLoadingIndicator?: boolean;
+      apiResponseHandlers?: Array<(apiResponse?: any) => any | Promise<any>>;
+    }
   ) => {
     try {
-      setIsLoading(true);
+      if (shouldShowRequestLoadingIndicator) setIsLoading(true);
       let apiResponse = await apiServiceMethod();
       setIsLoading(false);
 
@@ -61,7 +44,7 @@ export const useApiService = () => {
       }
 
       return apiResponse;
-    } catch (err: ErrorLike) {
+    } catch (err: unknown) {
       setIsLoading(false);
       setError(getTypeSafeErr(err, "Some error occurred, please try again later.").message);
     }
@@ -69,11 +52,8 @@ export const useApiService = () => {
 
   return {
     handleApiServiceRequest,
-    checkApiResponse,
+    checkApiResponseForAuthToken,
     setIsLoading,
     setError,
-    processAuthToken,
-    removeAuthToken,
-    processPreFetchedUserItems
   };
 };
--- src/hooks/useStripeService.ts
+++ src/hooks/useStripeService.ts
@@ -1,32 +1,41 @@
-import { stripeService } from "@services";
+import { stripeService, StripeServiceLinkResponse } from "@services/stripeService";
 import { useApiService } from "./useApiService";
+import type { SubmitPaymentForSubscriptionParams } from "@services/stripeService";
 
 export const useStripeService = () => {
-  const { handleApiServiceRequest, checkApiResponse } = useApiService();
+  const { handleApiServiceRequest, checkApiResponseForAuthToken } = useApiService();
+
+  const checkApiResponseForStripeLink = (apiResponse?: Partial<StripeServiceLinkResponse>) => {
+    if (typeof apiResponse?.stripeLink === "string") {
+      window.open(apiResponse.stripeLink);
+    }
+  };
 
   return {
     submitPaymentForSubscription: async (
-      paymentArgs: Parameters<typeof stripeService.submitPaymentForSubscription>[0]
+      paymentArgs: SubmitPaymentForSubscriptionParams
     ): Promise<{ success: boolean }> => {
       return await handleApiServiceRequest(
         async () => stripeService.submitPaymentForSubscription(paymentArgs),
-        [checkApiResponse.forAuthToken]
+        {
+          apiResponseHandlers: [checkApiResponseForAuthToken],
+        }
       );
     },
     getCustomerPortalLink: async () => {
-      await handleApiServiceRequest(stripeService.getCustomerPortalLink, [
-        checkApiResponse.forStripeLink
-      ]);
+      await handleApiServiceRequest(stripeService.getCustomerPortalLink, {
+        apiResponseHandlers: [checkApiResponseForStripeLink],
+      });
     },
     getConnectOnboardingLink: async () => {
-      await handleApiServiceRequest(stripeService.getConnectOnboardingLink, [
-        checkApiResponse.forStripeLink
-      ]);
+      await handleApiServiceRequest(stripeService.getConnectOnboardingLink, {
+        apiResponseHandlers: [checkApiResponseForAuthToken],
+      });
     },
     getConnectDashboardLink: async () => {
-      await handleApiServiceRequest(stripeService.getConnectDashboardLink, [
-        checkApiResponse.forStripeLink
-      ]);
-    }
+      await handleApiServiceRequest(stripeService.getConnectDashboardLink, {
+        apiResponseHandlers: [checkApiResponseForAuthToken],
+      });
+    },
   };
 };
