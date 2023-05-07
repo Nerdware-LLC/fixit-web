2bfe94a6dfe9d27b49b9e14e55fdcb0b7645cb7a
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 08:59:53 -0400

    refactor: rm exports of rm'd files

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,29 +1,5 @@
-import { INVOICE_CONSTANTS } from "./Invoice.types";
-import { USER_SUBSCRIPTION_CONSTANTS } from "./UserSubscription.types";
-import { WORK_ORDER_CONSTANTS } from "./WorkOrder.types";
-
-// Expose 1 object for all enums/constants
-export const CONSTANTS = {
-  INVOICE: INVOICE_CONSTANTS,
-  USER_SUBSCRIPTION: USER_SUBSCRIPTION_CONSTANTS,
-  WORK_ORDER: WORK_ORDER_CONSTANTS
-};
-
-// Export all API types:
-export * from "./AuthToken.types";
-export * from "./Contact.types";
-export * from "./FixitUser.types";
-export * from "./Invoice.types";
-export * from "./User.types";
-export * from "./UserLogin.types";
-export * from "./UserProfile.types";
-export * from "./UserStripeConnectAccount.types";
-export * from "./UserSubscription.types";
-export * from "./WorkOrder.types";
-export * from "./WorkOrderChecklist.types";
-
-// Export custom utility types:
-export * from "./FormValues.types";
-
-// Export custom and built-in prop-types:
-export * from "./propTypes";
+export * from "./AuthToken";
+export * from "./Invoice";
+export * from "./UserLogin";
+export * from "./UserSubscription";
+export * from "./WorkOrder";
