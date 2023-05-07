45600ff00b4d0201c86ab7b296834db1f4502a19
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 09:10:13 -0400

    feat: rm index file to enforce specific imports

--- src/graphql/index.ts
+++
@@ -1,6 +0,0 @@
-export { FRAGMENTS } from "./fragments";
-export { MUTATIONS } from "./mutations";
-export { QUERIES } from "./queries";
-
-// Re-export relevant apollo hooks
-export { useLazyQuery, useMutation, useQuery } from "@apollo/client/react/hooks";
