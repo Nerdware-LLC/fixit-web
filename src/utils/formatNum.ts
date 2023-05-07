09b956601307e169e76ab5853ded3b1f3ad76398
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 10:19:30 -0400

    style(prettier): implement new prettier rules

--- src/utils/formatNum.ts
+++ src/utils/formatNum.ts
@@ -1,14 +1,30 @@
-const intlCurrencyFmt = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" });
-const intlPercentageFmt = new Intl.NumberFormat("en-US", { style: "percent" });
+const currencyFmt = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 2,
+});
+
+const currencyRoundedFmt = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 0,
+});
+
+const percentageFmt = new Intl.NumberFormat("en-US", { style: "percent" });
 
 /**
- * These methods take as an argument a *WHOLE* integer (do not include any decimal places)
- * and returns a string representation based on expected units using the `en-US` locale.
+ * These methods take as an argument a *WHOLE* integer (do not include any decimal
+ * places), divides it by 100, and returns a string representation based on expected
+ * units using the `en-US` locale.
  *
- * - toCurrencyStr `123456` becomes `"$1,234.56"`
- * - toPercentageStr `123` becomes `"123%"`
+ * | Method                 | Arg      | Output        |
+ * | :--------------------- | :------- | :------------ |
+ * | `toCurrencyStr`        | `123456` | `"$1,234.56"` |
+ * | `toCurrencyRoundedStr` | `123456` | `"$1,235"`    |
+ * | `toPercentageStr`      | `1234`   | `"1,234%"`    |
  */
 export const formatNum = {
-  toCurrencyStr: (num: number) => intlCurrencyFmt.format(num / 100),
-  toPercentageStr: (num: number) => intlPercentageFmt.format(num)
+  toCurrencyStr: (num: number) => currencyFmt.format(num / 100),
+  toCurrencyRoundedStr: (num: number) => currencyRoundedFmt.format(num / 100),
+  toPercentageStr: (num: number) => percentageFmt.format(num / 100),
 };
