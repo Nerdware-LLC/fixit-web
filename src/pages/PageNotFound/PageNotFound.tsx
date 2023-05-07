0cb1b44636cd160ced0736c0f268a831b5c295f4
trevor-anderson <T.AndersonProperty@gmail.com>
2023-05-07 14:57:38 -0400

    feat: update PageNotFound, add DragonIcon

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,37 +1,169 @@
+import { useNavigate } from "react-router-dom";
+import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
-import Text from "@mui/material/Typography";
+import Divider, { dividerClasses } from "@mui/material/Divider";
+import Text, { typographyClasses } from "@mui/material/Typography";
 import { useWebViewContext } from "@app/WebViewContext/useWebViewContext";
-
-// FIXME Update PageNotFound to work with regular Web pages
+import { DragonIcon } from "@components/Icons/DragonIcon";
+import { Anchor } from "@components/Navigation/Anchor";
+import { Link } from "@components/Navigation/Link";
 
 export const PageNotFound = () => {
-  const { webViewPostMessage } = useWebViewContext(); // <-- will this throw if context not exists? maybe always have context, set to null if not a RN webview.
+  const nav = useNavigate();
+  const { isAppWithinWebView, webViewPostMessage } = useWebViewContext();
 
-  const exitWebViewWithError = () => {
-    webViewPostMessage({ error: "INVALID_ROUTE" });
+  const handleClickButton = () => {
+    if (isAppWithinWebView) webViewPostMessage({ error: "INVALID_ROUTE" });
+    else nav("/");
   };
 
   return (
-    <div
-      style={{
-        height: "100%",
-        padding: "30vh 15vw",
-        textAlign: "left",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-around",
-        alignItems: "center",
-      }}
-    >
-      <Text variant="h3">Whoops - this page doesn&apos;t seem to exist!</Text>
-      <Text>❌ 404 - File or directory not found.</Text>
-      <Text>
-        The resource you are looking for might have been removed, had its name changed, or is
-        temporarily unavailable.
-      </Text>
-      <Button onClick={exitWebViewWithError} style={{ alignSelf: "center" }} size="large">
-        Take Me Back to Civilization!
-      </Button>
-    </div>
+    <StyledDiv>
+      <div className={classNames.headerContainer}>
+        <Text variant="h2">
+          <span className={classNames.text404}>404</span>Whoops - this page doesn&apos;t seem to
+          exist!
+        </Text>
+      </div>
+      <div className={classNames.bodyContainer}>
+        <div>
+          <Text>Looking for something in particular? Hopefully one of these links will help:</Text>
+          <ul>
+            {[
+              { to: "/", label: "Home" },
+              { to: "/pricing", label: "Pricing" },
+              { to: "/privacy", label: "Privacy Policy" },
+              { to: "/tos", label: "Terms of Service" },
+            ].map(({ to, label }, index) => (
+              <>
+                {index !== 0 && <Divider orientation="vertical" flexItem />}
+                <li key={to}>
+                  <Link to={to}>{label}</Link>
+                </li>
+              </>
+            ))}
+          </ul>
+        </div>
+        <Text>
+          Sure,{` `}
+          <Anchor href="https://www.sparknotes.com/lit/fellowship/quotes/page/4/#:~:text=Quote%204,whether%20Strider%20is%20indeed%20Aragorn.">
+            "Not all those who wander are lost"
+          </Anchor>
+          {`  `}
+          and all that, but <i>you</i> - my friend - have wandered right off the map and straight
+          into <em>"There be dragons" </em> territory! <DragonIcon />
+        </Text>
+      </div>
+      <div className={classNames.buttonContainer}>
+        <Button onClick={handleClickButton} size="large">
+          Take me home!
+        </Button>
+      </div>
+    </StyledDiv>
   );
 };
+
+const classNames = {
+  text404: "page-not-found-text-404",
+  headerContainer: "page-not-found-header-container",
+  bodyContainer: "page-not-found-body-container",
+  buttonContainer: "page-not-found-button-container",
+};
+
+const StyledDiv = styled("div")(({ theme: { breakpoints, variables } }) => ({
+  height: "100%",
+  padding: "3rem 2rem",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "flex-start",
+  gap: "2rem",
+  textAlign: "center",
+
+  [breakpoints.up("md")]: {
+    justifyContent: "center",
+    padding: "4rem",
+    gap: "4rem",
+  },
+
+  // ALL CHILD DIV-CONTAINERS (low-specificity)
+  "& > div": {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    maxWidth: "66rem",
+  },
+
+  // HEADER
+  [`& > div.${classNames.headerContainer} > h2.${typographyClasses.root}`]: {
+    ...(variables.isMobilePageLayout
+      ? { fontSize: "2rem", lineHeight: "2.25rem" }
+      : { fontSize: "3.25rem", lineHeight: "3.25rem" }),
+    // "404" text
+    [`& .${classNames.text404}`]: {
+      opacity: 0.3,
+      marginRight: "1rem",
+    },
+  },
+
+  // BODY-CONTAINER
+  [`& > div.${classNames.bodyContainer}`]: {
+    flexDirection: "column",
+    gap: "inherit",
+    ...(variables.isMobilePageLayout
+      ? { fontSize: "1rem", lineHeight: "1.5rem" }
+      : { fontSize: "1.5rem", lineHeight: "2rem" }),
+
+    [`& .${typographyClasses.root}`]: {
+      fontSize: "inherit",
+      lineHeight: "inherit",
+    },
+
+    "& > div > ul": {
+      margin: "0.5rem 0 0 0",
+      padding: 0,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: variables.isMobilePageLayout ? "column" : "row",
+      "& > li": {
+        listStyleType: "none",
+        margin: "0 1rem",
+        "& > a": {
+          textDecoration: "underline",
+        },
+      },
+      [`& > .${dividerClasses.root}`]: {
+        ...(variables.isMobilePageLayout && { display: "none" }),
+      },
+    },
+
+    [`& > .${typographyClasses.root}`]: {
+      whiteSpace: "pre-wrap",
+
+      "& a,em": {
+        whiteSpace: "nowrap",
+        fontStyle: "italic",
+      },
+      "& svg": {
+        ...(variables.isMobilePageLayout
+          ? {
+              height: "1rem",
+              marginLeft: "0.15rem",
+              transform: "translateY(2px)",
+            }
+          : {
+              height: "1.5rem",
+              marginLeft: "0.35rem",
+              transform: "translateY(3px)",
+            }),
+      },
+    },
+  },
+
+  // BUTTON
+  [`& > div.${classNames.buttonContainer} > button`]: {
+    marginTop: "0.5rem",
+    fontStyle: "italic",
+  },
+}));
