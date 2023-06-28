41825772d1f616a672d5a66177a1d006772f97cc
trevor-anderson <T.AndersonProperty@gmail.com>
2023-06-28 11:39:39 -0400

    fix: update old jest references to vi

--- src/utils/__mocks__/storage.ts
+++ src/utils/__mocks__/storage.ts
@@ -1,3 +1,4 @@
+import { vi, beforeEach } from "vitest";
 import {
   storage as _storage,
   type LocalStorageWrapperKey,
@@ -18,17 +19,17 @@ export const storage = _storage.KEYS.reduce(
   (acc, key) => ({
     ...acc,
     [key]: {
-      get: jest.fn(() => {
+      get: vi.fn(() => {
         return _mockLocalStorage?.[key] ?? null;
       }),
-      set: jest.fn((str) => {
+      set: vi.fn((str) => {
         _mockLocalStorage[key] = str;
       }),
-      setDefaultIfEmpty: jest.fn((defaultValue) => {
+      setDefaultIfEmpty: vi.fn((defaultValue) => {
         const currentlyStoredValue = _mockLocalStorage?.[key] ?? null;
         if (currentlyStoredValue === null) _mockLocalStorage[key] = defaultValue;
       }),
-      remove: jest.fn((str) => {
+      remove: vi.fn(() => {
         delete _mockLocalStorage[key];
       }),
     },
