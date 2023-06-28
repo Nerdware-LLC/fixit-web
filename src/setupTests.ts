605f63c8c3d777803450e444aaae5d7d7931b4c9
trevor-anderson <T.AndersonProperty@gmail.com>
2023-06-28 09:22:25 -0400

    feat: mv setupTests to tests dir, update for Vitest

--- src/setupTests.ts
+++
@@ -1,4 +0,0 @@
-// This file corresponds to Jest config "setupFilesAfterEnv"
-// react-testing-library renders components to document.body
-// jest-dom adds custom jest matchers for test assertions on DOM nodes
-import "@testing-library/jest-dom";
