043b1fb0709eebdc2934a211d2b88662cfc1578d
trevor-anderson <T.AndersonProperty@gmail.com>
2023-06-28 10:16:40 -0400

    fix: ensure default viewContact logic is skipped if contact.id matches user

--- src/components/Avatar/ContactAvatar.tsx
+++ src/components/Avatar/ContactAvatar.tsx
@@ -1,24 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import Tooltip from "@mui/material/Tooltip";
+import { authenticatedUserStore } from "@cache/authenticatedUserStore";
 import { Avatar, type AvatarProps } from "./Avatar";
 import type { Profile } from "@graphql/types";
 
 /**
  * Will display an Avatar along with the Contact's `displayName`
  * - If `viewContactOnClick` is not explicitly set to false, onClick event
- *   handler will nav to `/home/contacts/:id`
- *   > Typically, `viewContactOnClick` will be
+ *   handler will nav to `/home/contacts/:id` if `contact.id` is not the
+ *   authenticated User's ID.
+ *   > If set explicitly, typically `viewContactOnClick` will be
  *   > - `TRUE  if contact={item.assignedTo}`
  *   > - `FALSE if contact={item.createdBy}`
  * - Can set `showDisplayName` = false if displayName is undesirable.
  */
 export const ContactAvatar = ({
   contact,
-  viewContactOnClick = true,
+  viewContactOnClick,
   showDisplayName = true,
   ...avatarProps
 }: ContactAvatarProps) => {
   const nav = useNavigate();
+  const { id: authenticatedUserID } = authenticatedUserStore.useSubToStore();
+
+  viewContactOnClick ??= contact.id !== authenticatedUserID;
 
   const commonAvatarProps = {
     profile: contact.profile,
