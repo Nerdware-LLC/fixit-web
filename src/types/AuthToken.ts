e79ed5aba100fcffa1037febca023cc945851868
trevor-anderson <T.AndersonProperty@gmail.com>
2023-06-28 11:44:09 -0400

    feat: add 'createdAt' and 'updatedAt' fields to AuthTokenPayload

--- src/types/AuthToken.ts
+++ src/types/AuthToken.ts
@@ -2,7 +2,10 @@ import type { User, UserStripeConnectAccount, UserSubscription } from "@graphql/
 import type { Simplify } from "type-fest";
 
 export type AuthTokenPayload = Simplify<
-  Pick<User, "id" | "handle" | "phone" | "profile" | "stripeCustomerID"> & {
+  Pick<
+    User,
+    "id" | "handle" | "phone" | "profile" | "stripeCustomerID" | "createdAt" | "updatedAt"
+  > & {
     email: string;
     stripeConnectAccount: Pick<
       UserStripeConnectAccount,
