59df6777c8d4621d6344486ed15ecf7823270de2
trevor-anderson <T.AndersonProperty@gmail.com>
2023-06-28 09:35:25 -0400

    build(types): add props type

--- src/app/DateTimeLocalizationProvider.tsx
+++ src/app/DateTimeLocalizationProvider.tsx
@@ -8,18 +8,19 @@ import {
   LocalizationProvider as MuiLocalizationProvider,
   type LocalizationProviderProps,
 } from "@mui/x-date-pickers/LocalizationProvider";
+import type { Except } from "type-fest";
 
 /**
  * This component provides date/time localization functionality using the Mui
  * `LocalizationProvider` with the `dayjs` adapter. This component is
  * necessary to run the Mui DatePicker/DateTimePicker components.
  *
- * > Currently, only the `en` locale is supported.
+ * > Currently, only the `"en"` locale is supported.
  */
 export const DateTimeLocalizationProvider = ({
   children,
   ...props
-}: LocalizationProviderProps<unknown>) => (
+}: DateTimeLocalizationProviderProps) => (
   <MuiLocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en" {...props}>
     {children}
   </MuiLocalizationProvider>
@@ -30,3 +31,8 @@ export const DateTimeLocalizationProvider = ({
 dayjs.extend(advancedFormatDayJsPlugin); // provides 1-24 hour format used by logger util
 dayjs.extend(isBetweenDayJsPlugin); // provides isBetween() method used by Dashboard itemDataParsers
 dayjs.extend(localizedFormatDayJsPlugin); // provides local date format used in CheckoutPage
+
+export type DateTimeLocalizationProviderProps = Except<
+  LocalizationProviderProps<unknown, "en">,
+  "dateAdapter" | "adapterLocale"
+>;
