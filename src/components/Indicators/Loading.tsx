d073b8ee6a954bb3fabe68c5fe0d8ebe3e4f455a
trevor-anderson <T.AndersonProperty@gmail.com>
2023-06-28 11:06:40 -0400

    feat: add ability to pass props to Loading comp

--- src/components/Indicators/Loading.tsx
+++ src/components/Indicators/Loading.tsx
@@ -1,8 +1,18 @@
-import CircularProgress from "@mui/material/CircularProgress";
-import { IndicatorContainer } from "./IndicatorContainer";
+import CircularProgress, { type CircularProgressProps } from "@mui/material/CircularProgress";
+import { IndicatorContainer, type IndicatorContainerProps } from "./IndicatorContainer";
 
-export const Loading = () => (
-  <IndicatorContainer>
-    <CircularProgress color="primary" size="7rem" thickness={4.5} />
+export const Loading = ({
+  color = "primary",
+  size = "7rem",
+  thickness = 4.5,
+  IndicatorContainerProps = {},
+  ...circularProgressProps
+}: LoadingProps) => (
+  <IndicatorContainer {...IndicatorContainerProps}>
+    <CircularProgress color={color} size={size} thickness={thickness} {...circularProgressProps} />
   </IndicatorContainer>
 );
+
+export type LoadingProps = CircularProgressProps & {
+  IndicatorContainerProps?: IndicatorContainerProps;
+};
