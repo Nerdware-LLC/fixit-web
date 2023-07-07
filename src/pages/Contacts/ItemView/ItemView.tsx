76a0c973f2dcf52cf4c8b1b6e7d6ad797006e04f
trevor-anderson <T.AndersonProperty@gmail.com>
2023-07-07 11:06:14 -0400

    docs: add IDEA tag for new ContactItemView features

--- src/pages/Contacts/ItemView/ItemView.tsx
+++ src/pages/Contacts/ItemView/ItemView.tsx
@@ -5,22 +5,21 @@ import { Loading } from "@components/Indicators/Loading";
 import { QUERIES } from "@graphql/queries";
 import { ProfileViewLayout } from "@layouts/ProfileViewLayout";
 
-/* TODO Add to ContactItemView:
-
-  - "Pay this Contact" button
-  - "Send Payment Request" button
-  - WO history and/or INV history between User and Contact
-*/
-
+/**
+ * // IDEA Add to ContactItemView:
+ * - "Pay this Contact" button
+ * - "Send Payment Request" button
+ * - WO history and/or INV history between User and Contact
+ */
 export const ContactItemView = () => {
-  const { id } = useParams();
+  const { id: contactID } = useParams();
 
-  const { data, loading, error, networkStatus } = useQuery(QUERIES.CONTACT, {
-    variables: { contactID: id ?? "" },
-    skip: !id,
+  const { data, loading, error } = useQuery(QUERIES.CONTACT, {
+    variables: { contactID: contactID ?? "" },
+    skip: !contactID,
   });
 
-  return loading || networkStatus === 4 || !data?.contact ? (
+  return loading || !data?.contact ? (
     <Loading />
   ) : error ? (
     <Error error={error} />
