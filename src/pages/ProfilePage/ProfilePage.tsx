7e10dfc1db9ea82f89e16f925a9cc5f028ca2f7a
trevor-anderson <T.AndersonProperty@gmail.com>
2023-07-07 11:08:29 -0400

    docs: add IDEA tag for ProfilePage features

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,12 +1,11 @@
 import { authenticatedUserStore } from "@cache/authenticatedUserStore";
 import { ProfileViewLayout } from "@layouts/ProfileViewLayout";
 
-/* TODO Add to ProfilePage:
-
-  - Button/form to update DisplayName
-  - Button/form to update profile pic
-*/
-
+/**
+ * // IDEA Add to ProfilePage:
+ * - Button/form to update DisplayName
+ * - Button/form to update profile pic
+ */
 export const ProfilePage = () => {
   const user = authenticatedUserStore.useSubToStore();
 
