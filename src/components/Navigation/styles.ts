1e333efe70c6adcdaf578b5e47a2c9edfa9d4a2f
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 11:41:14 -0500

    refactor: add SxProps, rename fn to better reflect purpose

--- src/components/Navigation/styles.ts
+++ src/components/Navigation/styles.ts
@@ -1,18 +1,18 @@
-import type { Theme } from "@mui/material/styles";
+import type { Theme, SxProps } from "@mui/material/styles";
 
 /**
- * Default Link and anchor styles
+ * Default styles for the `Link` and `Anchor` components.
  */
-export const defaultLinkStyles = ({
-  theme,
-  themecolor = "info",
-}: {
-  theme: Theme;
-  themecolor?: "primary" | "secondary" | "info";
-}) => ({
-  color: theme.palette[themecolor].main,
-  textDecoration: "none",
-  display: "inline-flex",
-  alignItems: "center",
-  verticalAlign: "middle",
-});
+export const getDefaultLinkStyles = ({ theme: { palette } }: { theme: Theme }) =>
+  ({
+    color: palette.info.main,
+    textDecoration: "none",
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+
+    "&:hover": {
+      cursor: "pointer",
+      opacity: 0.7,
+    },
+  }) satisfies SxProps<Theme>;
