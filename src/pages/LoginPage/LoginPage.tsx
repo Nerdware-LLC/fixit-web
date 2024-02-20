a0b38c4661aefd399a5bc41244d13778bc2fb7fe
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 12:42:21 -0500

    feat: rm useEffect, impl route constants

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,61 +1,31 @@
-import { useEffect } from "react";
-import { useLocation, Link } from "react-router-dom";
-import { toast } from "react-toastify";
+import Box from "@mui/material/Box";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import { AuthPageLayout, authPageLayoutClassNames } from "@layouts/AuthPageLayout";
+import { Link } from "@/components/Navigation";
+import { AuthPageLayout } from "@/layouts/AuthPageLayout";
+import { APP_PATHS } from "@/routes/appPaths";
 import { LoginForm } from "./LoginForm";
 
 /**
- * **LoginPage**
- * - Wrapped within `AuthPagesLayout` in RootAppRouter
- * - Renders when path is "/login"
+ * **LoginPage** - renders when path is "/login"
  */
-export const LoginPage = () => {
-  const { state: locationState } = useLocation();
-
-  useEffect(() => {
-    if (locationState?.isRedirect === true) {
-      toast.info(
-        "You must be logged in to perform this action. Please sign in or create an account.",
-        { toastId: "auth-required-login-redirect" }
-      );
-    }
-  }, [locationState]);
-
-  return (
-    <AuthPageLayout
-      pageTitle="User Login"
-      sx={({ palette }) => ({
-        [`& > .${authPageLayoutClassNames.childrenContainer}`]: {
-          minHeight: "25vh",
-
-          [`& > #${elementIDs.signupLinkContainer}`]: {
-            color: palette.info.main,
-            alignSelf: "center",
-            whiteSpace: "pre-line",
-            marginTop: "1.5rem",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-
-            '& a[href="/register"]': {
-              color: palette.info.main,
-              alignSelf: "center",
-              whiteSpace: "pre-line",
-            },
-          },
-        },
-      })}
+export const LoginPage = () => (
+  <AuthPageLayout pageTitle="User Login">
+    <LoginForm />
+    <Box
+      style={{
+        alignSelf: "center",
+        whiteSpace: "pre-line",
+        marginTop: "1.5rem",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+      }}
     >
-      <LoginForm />
-      <span id={elementIDs.signupLinkContainer}>
-        <Link to="/register">Not an existing user? Sign up now </Link>
-        <ChevronRightIcon />
-      </span>
-    </AuthPageLayout>
-  );
-};
+      <Link to={APP_PATHS.REGISTER}>Not an existing user? Sign up now </Link>
+      <ChevronRightIcon color="info" style={{ transform: "translateY(1px)" }} />
+    </Box>
+  </AuthPageLayout>
+);
 
-export const elementIDs = {
-  signupLinkContainer: "login-page-signup-link-container",
-};
+// Exported as "Component" for react-router-dom lazy loading
+export const Component = LoginPage;
