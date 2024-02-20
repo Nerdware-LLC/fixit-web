d2967fb628de5f4dc9c119700dd9d286d2b31ab1
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 11:09:19 -0500

    feat: add export of DataDisplay classNames

--- src/components/DataDisplay/classNames.ts
+++ src/components/DataDisplay/classNames.ts
@@ -1,17 +1,23 @@
-export const itemDetailsClassNames = {
-  itemDetails: "item-details",
-  container: "item-details-container",
-  header: "item-details-header",
-  content: "item-details-content",
-
-  itemDetailsGroup: "item-details-group",
-  groupContainer: "item-details-group-container",
-  groupHeader: "item-details-group-header",
-  groupContent: "item-details-group-content",
-
+/**
+ * Class names for `DataDisplay` components (src/components/DataDisplay/).
+ */
+export const dataDisplayClassNames = {
+  // ItemDetails
+  root: "item-details__root",
+  header: "item-details__header",
+  content: "item-details__content",
+  // ItemDetailsGroup
+  groupRoot: "item-details-group__root",
+  groupHeader: "item-details-group__header",
+  groupContent: "item-details-group__content",
+  // ItemDetailsHeader
   label: "item-details-label",
-
+  // LocationDetails
   locationDetails: "location-details",
-  locationDetailsAddressText: "location-details-address-text",
-  locationDetailsMapAnchor: "location-details-map-anchor",
-};
+  locationDetailsAddressText: "location-details__address-text",
+  locationDetailsMapAnchor: "location-details__map-anchor",
+  // InvoiceWorkOrderDetails
+  invoiceWorkOrderDetailsRoot: "invoice-work-order-details__root",
+  // HelpInfoIcon
+  helpInfoIcon: "help-info-icon",
+} as const;
--- src/components/DataDisplay/index.ts
+++ src/components/DataDisplay/index.ts
@@ -1,2 +1,8 @@
+export * from "./ItemDetails";
+export * from "./ItemDetailsGroup";
+
+export * from "./HelpInfoIcon";
+export * from "./InvoiceWorkOrderDetails";
+export * from "./LocationDetails";
+
 export * from "./classNames";
-export * from "./types";
