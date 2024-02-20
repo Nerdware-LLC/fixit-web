abc858eebc94aacd2f61d9e36bd6f35c0f193c72
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 12:09:44 -0500

    feat: impl helpers,constants,stores

--- src/layouts/CoreItemsListView/classNames.ts
+++ src/layouts/CoreItemsListView/classNames.ts
@@ -1,21 +1,9 @@
-import { listClasses as muiListClassNames } from "@mui/material/List";
-import { dataGridClassNames } from "@components/DataGrid";
-
+/**
+ * Class names for `CoreItemsListView` components.
+ */
 export const coreItemsListViewClassNames = {
-  root: "core-items-list-view-root",
-  contentContainer: "core-items-list-view-content-container",
-  listsDivider: "core-items-list-view-lists-divider",
-  listContainer: "core-items-list-view-list-container",
-  list: "core-items-list-view-list",
-  muiList: { ...muiListClassNames },
-  dataGrid: { ...dataGridClassNames },
-};
-
-export const coreListItemLayoutClassNames = {
-  root: "list-view-item-root",
-  leftContentContainer: "list-view-item-left-content-container",
-  avatar: "list-view-item-avatar",
-  childrenContentContainer: "list-view-item-children-content-container",
-  createdAtText: "list-view-item-created-at-text",
-  statusText: "list-view-item-status-text",
-};
+  root: "core-items-list-view__root",
+  contentContainer: "core-items-list-view__content-container",
+  listsDivider: "core-items-list-view__lists-divider",
+  listContainer: "core-items-list-view__list-container",
+} as const;
