96ff6eba31e7a7dd07a59b9681ba07c8b35c4594
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 11:47:17 -0500

    build: add StripeForm Storybook stories

---
+++ src/components/StripeForm/StripeForm.stories.tsx
@@ -0,0 +1,36 @@
+import { FetchStateContextProvider } from "@/app/FetchStateContext";
+import { authenticatedUserStore } from "@/stores/authenticatedUserStore";
+import { STATIC_MOCK_USERS } from "@/tests/mockItems/staticMockUsers";
+import { StripeForm } from "./StripeForm";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Components/StripeForm",
+  component: StripeForm,
+  decorators: [
+    (Story) => (
+      <FetchStateContextProvider>
+        <Story />
+      </FetchStateContextProvider>
+    ),
+  ],
+} satisfies Meta<typeof StripeForm>;
+
+export default meta;
+
+///////////////////////////////////////////////////////////
+// STORIES
+
+type Story = StoryObj<typeof meta>;
+
+authenticatedUserStore.set(STATIC_MOCK_USERS.Guy_McPerson as any);
+
+export const PaymentInputDemo = {
+  args: {
+    stripeElementsOptions: {
+      mode: "subscription",
+      currency: "usd",
+      amount: 1000,
+    },
+  },
+} satisfies Story;
