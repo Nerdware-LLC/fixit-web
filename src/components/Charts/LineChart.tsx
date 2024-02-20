491402dc0a55ea5d0aa902cf538555ac4ae0a980
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 10:57:41 -0500

    refactor: impl @nerdware utils, dedupe configs

--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -1,10 +1,17 @@
 import { Suspense, lazy } from "react";
+import { isString } from "@nerdware/ts-type-safety-utils";
+import deepMerge from "lodash.merge";
+import { useTheme, alpha } from "@mui/material/styles";
 import Box, { type BoxProps } from "@mui/material/Box";
-import CircularProgress from "@mui/material/CircularProgress";
-import type { ChartData, ChartOptions } from "chart.js";
+import { usePageLayoutContext } from "@/app/PageLayoutContext";
+import { Loading } from "@/components/Indicators/Loading";
+import type { ChartData, ChartOptions, DefaultDataPoint } from "chart.js";
+import type { ChartProps } from "react-chartjs-2/dist/types";
+import type { Simplify } from "type-fest";
 
-// prettier-ignore
-const Line = lazy(() => import(/* webpackChunkName: "ChartJS-LineChart" */ "./chartJS.registerLineChart"));
+const Line = lazy(
+  () => import(/* webpackChunkName: "ChartJS-LineChart" */ "./chartJS.registerLineChart")
+);
 
 /**
  * A react-chartjs-2 Line Chart which dynamically imports the bulky ChartJS bundle
@@ -17,55 +24,131 @@ const Line = lazy(() => import(/* webpackChunkName: "ChartJS-LineChart" */ "./ch
  *   2. `props.options.plugins.title.text` value if it's _truthy_
  *   3. `"Fixit Line Chart"` is used as a fallback if the other values are _falsey_
  *
+ * > If `options.scales` is provided, only `x` and `y` axes are valid.
+ *   Custom values for `xAxisID` and `yAxisID` are not supported.
+ *
  * @docs
  * - [react-chartjs-2 Line Chart](https://react-chartjs-2.netlify.app/components/line)
  * - [ChartJS Line Chart](https://www.chartjs.org/docs/latest/charts/line.html)
  * - [ChartJS a11y](https://www.chartjs.org/docs/latest/general/accessibility.html)
  */
-export const LineChart = ({
+export const LineChart = <
+  TData extends DefaultDataPoint<"line"> = DefaultDataPoint<"line">,
+  TLabel = unknown,
+>({
+  title,
   data,
+  options = {},
   height = "100%",
   width = "100%",
-  options = {},
-  sx,
   // ARIA attributes:
   role = "img",
-  "aria-label": ariaLabel = `${options?.plugins?.title?.text || ""}` || "Fixit Line Chart",
-  ...props
-}: LineChartProps) => (
-  <Box
-    className={lineChartClassNames.container}
-    sx={sx}
-    style={{
-      height: "100%",
-      width: "100%",
-      display: "flex",
-      alignItems: "space-around",
-      justifyContent: "space-around",
-    }}
-  >
-    <Suspense fallback={<CircularProgress />}>
-      <Line
-        data={data}
-        height={height}
-        width={width}
-        options={options}
-        role={role}
-        aria-label={ariaLabel}
-        {...props}
-      />
-    </Suspense>
-  </Box>
-);
+  "aria-label": ariaLabel,
+  // Box container props:
+  sx,
+  ref,
+  ...lineChartProps
+}: LineChartProps<TData, TLabel>) => {
+  const { isMobilePageLayout } = usePageLayoutContext();
+  const { palette } = useTheme();
 
-export const lineChartClassNames = {
-  container: "line-chart-container",
+  const defaultTitleAndLegendFontColor = alpha(palette.text.primary, 0.75);
+
+  // Default styles for both X and Y axes elements with theme/palette colors:
+  const xyAxesDefaultStyles = {
+    ticks: {
+      padding: 10,
+      color: alpha(palette.text.primary, 0.7),
+      maxRotation: 0,
+    },
+    ...(palette.mode === "dark" && {
+      grid: { color: alpha(palette.text.primary, 0.07) },
+      border: { color: alpha(palette.text.primary, 0.09) },
+    }),
+  };
+
+  // If `title` was not provided,  but `options.plugins.title.text` was, set `title` to that value:
+  if (!title) {
+    const maybeTitlePluginText = options?.plugins?.title?.text;
+    if (isString(maybeTitlePluginText)) title = maybeTitlePluginText;
+  }
+
+  // Merge user-provided options with default options:
+  const mergedOptions = deepMerge(
+    // Default options:
+    {
+      scales: {
+        x: { axis: "x", ...xyAxesDefaultStyles },
+        y: { axis: "y", ...xyAxesDefaultStyles },
+      },
+      plugins: {
+        title: {
+          text: title,
+          display: !!title,
+          color: defaultTitleAndLegendFontColor,
+        },
+        subtitle: {
+          display: true,
+          text: `${isMobilePageLayout ? "Tap" : "Click"} labels to show/hide data`,
+          // the above text can be hidden by setting `plugins.subtitle.display` to `false`
+        },
+        legend: {
+          labels: {
+            color: defaultTitleAndLegendFontColor,
+          },
+        },
+      },
+    },
+    // User-provided options:
+    options
+  );
+
+  return (
+    <Box
+      sx={sx}
+      ref={ref}
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        alignItems: "space-around",
+        justifyContent: "space-around",
+      }}
+    >
+      <Suspense fallback={<Loading />}>
+        <Line
+          data={data}
+          title={title}
+          height={height}
+          width={width}
+          role={role}
+          options={mergedOptions}
+          aria-label={ariaLabel || title || "Fixit Line Chart"}
+          {...lineChartProps}
+        />
+      </Suspense>
+    </Box>
+  );
 };
 
-export type LineChartProps = {
-  data: ChartData<"line">;
+export type LineChartProps<
+  TData extends DefaultDataPoint<"line"> = DefaultDataPoint<"line">,
+  TLabel = unknown,
+> = {
+  data: ChartData<"line", TData, TLabel>;
+  title?: string;
   height?: string;
   width?: string;
-  options?: ChartOptions<"line">;
-} & BoxProps &
-  Omit<React.ComponentProps<typeof Line>, "data" | "options" | "height" | "width">;
+  options?: Simplify<
+    Omit<ChartOptions<"line">, "scales"> & {
+      scales?: Partial<Record<"x" | "y", LineChartAxisOptions>>;
+    }
+  >;
+} & Pick<BoxProps, "sx" | "ref"> &
+  Omit<ChartProps<"line", TData, TLabel>, "type" | "data" | "options" | "height" | "width">;
+
+/**
+ * A type representing the {@link LineChart} component's `options.scales["x" | "y"]` objects
+ * used to configure the `x` and `y` axes.
+ */
+export type LineChartAxisOptions = Exclude<ChartOptions<"line">["scales"], undefined>[string];
