ce8cf20ffb6217503b72feef70a4387574427cf2
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 10:41:11 -0500

    feat: add `| SVGElement` to type constraint of T

--- src/hooks/useMaybeRef.ts
+++ src/hooks/useMaybeRef.ts
@@ -1,14 +1,9 @@
-import React, { useRef } from "react";
+import { useRef, type ForwardedRef } from "react";
 
 /**
  * Type guard hook which provides a fallback ref if parent does not forward a ref.
  */
-export const useMaybeRef = <T extends HTMLElement>(maybeRef?: React.ForwardedRef<MaybeRef<T>>) => {
+export const useMaybeRef = <T extends HTMLElement | SVGElement>(maybeRef?: ForwardedRef<T>) => {
   const localRef = useRef<T>(null);
-  return maybeRef || localRef;
+  return maybeRef ?? localRef;
 };
-
-/**
- * Generic typing for a nullable ref.
- */
-export type MaybeRef<T extends HTMLElement> = T | null;
