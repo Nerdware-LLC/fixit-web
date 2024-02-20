03f53eb4709f8f4e85f890bfb715a9211972c888
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 11:32:05 -0500

    build: add Form Storybook stories

---
+++ src/components/Form/FormControlButtons.stories.tsx
@@ -0,0 +1,32 @@
+import { object as yupObject, mixed as yupMixed } from "yup";
+import {
+  withFormDecorator,
+  withNavDecorator,
+  type FormDecoratorArgs,
+  type NavDecoratorArgs,
+} from "@/../.storybook/decorators";
+import { FormControlButtons, type FormControlButtonsProps } from "./FormControlButtons";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Components/Form/FormControlButtons",
+  component: FormControlButtons,
+  tags: ["autodocs"],
+  decorators: [withFormDecorator, withNavDecorator],
+} satisfies Meta<FormControlButtonsProps & FormDecoratorArgs & NavDecoratorArgs>;
+
+export default meta;
+
+///////////////////////////////////////////////////////////
+// STORIES
+
+type Story = StoryObj<typeof meta>;
+
+export const BasicDemo = {
+  args: {
+    _form_decorator_args: {
+      validationSchema: yupObject({ fooField: yupMixed().defined().nullable().default(null) }),
+      initialValues: { fooField: null },
+    },
+  },
+} satisfies Story;
---
+++ src/components/Form/FormSubmitButton.stories.tsx
@@ -0,0 +1,32 @@
+import { object as yupObject, mixed as yupMixed } from "yup";
+import { withFormDecorator, type FormDecoratorArgs } from "@/../.storybook/decorators";
+import { FormSubmitButton, type FormSubmitButtonProps } from "./FormSubmitButton";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Components/Form/FormSubmitButton",
+  component: FormSubmitButton,
+  tags: ["autodocs"],
+  decorators: [withFormDecorator],
+} satisfies Meta<FormSubmitButtonProps & FormDecoratorArgs>;
+
+export default meta;
+
+///////////////////////////////////////////////////////////
+// STORIES
+
+type Story = StoryObj<typeof meta>;
+
+export const BasicDemo = {
+  args: {
+    _form_decorator_args: {
+      validationSchema: yupObject({ fooField: yupMixed().defined().nullable().default(null) }),
+      initialValues: { fooField: null },
+      style: { display: "grid", placeItems: "center" },
+    },
+
+    style: {
+      width: "8rem",
+    },
+  },
+} satisfies Story;
