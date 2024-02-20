2ad17379a5ad7b8c1a37386bf23d3a55b0cc66f0
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 11:57:05 -0500

    build: update env-vars typedefs

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,9 +1,9 @@
-/// <reference types="vite/client" />
-
 interface ImportMetaEnv {
-  VITE_SENTRY_DSN?: string; // not present for test environment
-  VITE_API_PROTOCOL: string;
-  VITE_API_HOST: string;
-  VITE_STRIPE_PUBLISHABLE_KEY: string;
-  VITE_FIXIT_SUB_PROMO_CODES_JSON: string;
+  STORYBOOK?: boolean;
+  VITE_PROJECT_VERSION?: string;
+  VITE_SENTRY_DSN?: string;
+  VITE_API_PROTOCOL?: string;
+  VITE_API_HOST?: string;
+  VITE_API_BASE_PATH?: string;
+  VITE_STRIPE_PUBLISHABLE_KEY?: string;
 }
