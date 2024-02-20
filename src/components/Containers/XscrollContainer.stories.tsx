536cacb1604958e87cfdb7c646e599e39a117c04
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 11:02:54 -0500

    build: add Containers Storybook stories

---
+++ src/components/Containers/XscrollContainer.stories.tsx
@@ -0,0 +1,40 @@
+import Box from "@mui/material/Box";
+import Text from "@mui/material/Typography";
+import { XscrollContainer } from "./XscrollContainer";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Components/Containers/XscrollContainer",
+  component: XscrollContainer,
+} satisfies Meta<typeof XscrollContainer>;
+
+export default meta;
+
+///////////////////////////////////////////////////////////
+// STORIES
+
+type Story = StoryObj<typeof meta>;
+
+export const BasicDemo = {
+  render: () => (
+    <Box
+      style={{
+        margin: "auto",
+        maxHeight: "60%",
+        minWidth: "5rem",
+        width: "clamp(5rem, 100%, 30rem)",
+        maxWidth: "30rem",
+        padding: "1rem",
+        border: "1px dashed red",
+      }}
+    >
+      <XscrollContainer style={{ border: "1px dashed yellow" }}>
+        <Text style={{ whiteSpace: "nowrap" }}>
+          This long string of text takes up space to facilitate the demonstration of
+          XScollContainer's ability to scroll horizontally on any device viewport in a manner that
+          reflects the user's expectation of how horizontal scrolling should work.
+        </Text>
+      </XscrollContainer>
+    </Box>
+  ),
+} satisfies Story;
