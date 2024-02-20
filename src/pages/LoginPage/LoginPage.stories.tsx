0393abc918ce5d623125a73a83aeed5b415f4dc2
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 12:42:34 -0500

    build: add LoginPage Storybook stories

---
+++ src/pages/LoginPage/LoginPage.stories.tsx
@@ -0,0 +1,21 @@
+import { withRootAppLayoutDecorator } from "@/../.storybook/decorators";
+import { LoginPage } from "./LoginPage";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Pages/LoginPage",
+  component: LoginPage,
+  decorators: [withRootAppLayoutDecorator],
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof LoginPage>;
+
+export default meta;
+
+///////////////////////////////////////////////////////////
+// STORIES
+
+type Story = StoryObj<typeof meta>;
+
+export const BasicDemo = {} satisfies Story;
