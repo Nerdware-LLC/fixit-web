3ec96e0c276004f1443e8e22c6806d00966c2082
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 11:01:05 -0500

    refactor: impl base StyledChip comp

--- src/components/Chips/WorkOrderStatusChip.tsx
+++ src/components/Chips/WorkOrderStatusChip.tsx
@@ -1,32 +1,15 @@
-import Chip, { type ChipProps } from "@mui/material/Chip";
-import { svgIconClasses } from "@mui/material/SvgIcon";
-import { WO_STATUS_ICON_REACT_NODES } from "../Icons/WorkOrderStatusIcon";
-import type { WorkOrderStatus } from "@graphql/types";
-import type { SxProps } from "@mui/material/styles";
+import { WO_STATUS_ICONS_JSX } from "@/components/Icons/WorkOrderStatusIcon";
+import { StyledChip, type StyledChipProps } from "./StyledChip";
+import type { WorkOrderStatus } from "@/graphql/types";
+import type { Except } from "type-fest";
 
 /**
- * WorkOrder `status` Mui Chip
+ * {@link WorkOrderStatus|WorkOrder `status`} Mui Chip
  */
-export const WorkOrderStatusChip = ({ status, ...props }: WorkOrderStatusChipProps) => (
-  <Chip
-    label={status.replace(/_/g, " ")}
-    icon={WO_STATUS_ICON_REACT_NODES[status]}
-    sx={{
-      [`& svg.${svgIconClasses.root}`]: {
-        ...(status in STATUS_ICON_STYLES ? STATUS_ICON_STYLES[status] : {}),
-      },
-    }}
-    {...props}
-  />
+export const WorkOrderStatusChip = ({ status, ...chipProps }: WorkOrderStatusChipProps) => (
+  <StyledChip label={status.replace(/_/g, " ")} icon={WO_STATUS_ICONS_JSX[status]} {...chipProps} />
 );
-
-// Status-icon margin adjustments
-const STATUS_ICON_STYLES = {
-  UNASSIGNED: { margin: "0 0 0 0.5rem" },
-  ASSIGNED: { margin: "0 -0.25rem 0 0.75rem" },
-  DEFERRED: { margin: "0 -0.5rem 0 0.5rem" },
-} as Partial<Record<WorkOrderStatus, SxProps>>;
 
 export type WorkOrderStatusChipProps = {
   status: Extract<WorkOrderStatus, string>;
-} & Omit<ChipProps, "avatar" | "icon">;
+} & Except<StyledChipProps, "label" | "icon" | "children" | "avatar" | "component">;
