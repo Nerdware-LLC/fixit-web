4bac8301a2599905d5dd2d089ac464c9ab9733bc
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 11:33:32 -0500

    feat: add ref fwd, rm unused JSX exports

--- src/components/Icons/WorkOrderStatusIcon.tsx
+++ src/components/Icons/WorkOrderStatusIcon.tsx
@@ -1,35 +1,43 @@
-import SvgIcon, { type SvgIconProps } from "@mui/material/SvgIcon";
+import { forwardRef } from "react";
 import CompleteIcon from "@mui/icons-material/CheckCircleRounded";
 import DocumentIcon from "@mui/icons-material/Description";
 import EngineerCogsIcon from "@mui/icons-material/EngineeringRounded";
+import ExclamationMarkIcon from "@mui/icons-material/ErrorOutline";
+import EventBusyIcon from "@mui/icons-material/EventBusy";
 import ClockDottedOutlineIcon from "@mui/icons-material/HistoryToggleOffRounded";
-import NotInterestedIcon from "@mui/icons-material/NotInterested";
-import ExclamationMarkIcon from "@mui/icons-material/PriorityHighRounded";
-import type { WorkOrderStatus } from "@graphql/types";
+import { useMaybeRef } from "@/hooks/useMaybeRef";
+import type { WorkOrderStatus } from "@/graphql/types";
+import type { SvgIconProps } from "@mui/material/SvgIcon";
 
-export const WorkOrderStatusIcon = ({ status, ...props }: WorkOrderStatusIconProps) => {
-  const WOStatusIcon = WO_STATUS_ICONS[status];
+export const WorkOrderStatusIcon = forwardRef<SVGSVGElement, WorkOrderStatusIconProps>(
+  function WorkOrderStatusIcon({ status, ...svgIconProps }, ref) {
+    const svgRef = useMaybeRef(ref);
 
-  return <WOStatusIcon {...props} />;
-};
+    const StatusIcon = WO_STATUS_ICONS[status];
 
-export const WO_STATUS_ICONS: Record<WorkOrderStatus, typeof SvgIcon> = {
+    return <StatusIcon ref={svgRef} {...svgIconProps} />;
+  }
+);
+
+/**
+ * Map of {@link WorkOrderStatus|WorkOrder statuses} to their corresponding icon component.
+ */
+export const WO_STATUS_ICONS = {
   UNASSIGNED: DocumentIcon,
   ASSIGNED: EngineerCogsIcon,
   IN_PROGRESS: ClockDottedOutlineIcon,
   DEFERRED: ExclamationMarkIcon,
-  CANCELLED: NotInterestedIcon,
+  CANCELLED: EventBusyIcon,
   COMPLETE: CompleteIcon,
-};
+} as const satisfies Record<WorkOrderStatus, React.ComponentType>;
 
-export const WO_STATUS_ICON_REACT_NODES: Record<WorkOrderStatus, React.ReactElement> = {
-  UNASSIGNED: <DocumentIcon />,
-  ASSIGNED: <EngineerCogsIcon />,
-  IN_PROGRESS: <ClockDottedOutlineIcon />,
-  DEFERRED: <ExclamationMarkIcon />,
-  CANCELLED: <NotInterestedIcon />,
-  COMPLETE: <CompleteIcon />,
-};
+/**
+ * Map of {@link WorkOrderStatus|WorkOrder statuses} to their corresponding icon as JSX.
+ * > If you need to pass props to the icon, use {@link WO_STATUS_ICONS} instead.
+ */
+export const WO_STATUS_ICONS_JSX = Object.fromEntries(
+  Object.entries(WO_STATUS_ICONS).map(([status, Icon]) => [status, <Icon key={status} />])
+) as Record<WorkOrderStatus, JSX.Element>;
 
 export type WorkOrderStatusIconProps = {
   status: WorkOrderStatus;
