72729681fb56eb18b2d3b920eea7079eed7da326
trevor-anderson <trevor@nerdware.cloud>
2024-02-20 12:23:08 -0500

    build: update Vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,45 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import reactPlugin from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
-// import eslintPlugin from "vite-plugin-eslint";
 import svgrPlugin from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import GithubActionsReporter from "vitest-github-actions-reporter";
+import { coverageConfigDefaults } from "vitest/config";
 
 /**
  * @docs https://vitejs.dev/config/
  *
  * Plugins:
+ * - [React Plugin](https://github.com/vitejs/vite-plugin-react/tree/main/packages/plugin-react#readme)
  * - [SVGR Plugin](https://react-svgr.com/docs/options/)
  * - [Sentry plugin](https://docs.sentry.io/platforms/javascript/guides/react/sourcemaps/uploading/vite/)
  */
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
+  // Gather environment variables (these may or may not be present)
   const {
     VITE_API_PROTOCOL,
     VITE_API_BASE_URI,
-    VITE_SENTRY_AUTH_TOKEN
-  } = loadEnv(mode, process.cwd(), "VITE"); // prettier-ignore
+    VITE_SENTRY_AUTH_TOKEN,
+    VITE_NPM_LIFECYCLE_SCRIPT,
+  } = loadEnv(mode, process.cwd(), "VITE");
+
+  // For env-specific configs, ascertain info about the execution context:
+  const isProdBuild = command === "build" && mode === "production";
+  const isStorybookBuild = /storybook/.test(VITE_NPM_LIFECYCLE_SCRIPT ?? "");
 
   return {
     plugins: [
       reactPlugin({
         jsxImportSource: "@emotion/react",
-        babel: {
-          plugins: ["@emotion/babel-plugin"],
-        },
+        babel: { plugins: ["@emotion/babel-plugin"] },
       }),
-      // TODO Enable eslint plugin once they've updated the pkg to allow the new eslint config file
-      // eslintPlugin(),
       svgrPlugin({ svgrOptions: { icon: true } }),
       viteTsconfigPaths({
-        projects: [mode === "production" ? "./tsconfig.build.json" : "./tsconfig.json"],
+        projects: [isProdBuild && !isStorybookBuild ? "./tsconfig.build.json" : "./tsconfig.json"],
       }),
-      // The Sentry-vite plugin must be placed last after all other plugins
-      ...(/^(dev|prod)/i.test(mode) && !!VITE_SENTRY_AUTH_TOKEN
+      /* The Sentry-vite plugin uploads prod-build source maps to Sentry, and also adds
+      support for release management. It must be placed last after all other plugins.*/
+      ...(isProdBuild && !isStorybookBuild && !!VITE_SENTRY_AUTH_TOKEN
         ? [
             sentryVitePlugin({
               org: "nerdware-io",
@@ -62,29 +66,36 @@ export default defineConfig(({ mode }) => {
     },
 
     test: {
+      /* `restoreMocks` accomplishes the following:
+        - clears all spies of `spy.mock.calls` and `spy.mock.results` (same as clearMocks:true)
+        - removes any mocked implementations (same as mockReset:true)
+        - restores the original implementation so fns don't return undefined like with mockReset
+      */
+      restoreMocks: true,
       globals: true,
+      silent: true,
       environment: "jsdom",
+      server: {
+        deps: {
+          inline: ["vitest-canvas-mock"],
+        },
+      },
       include: ["**/?(*.){test,spec}.?(c|m)[tj]s?(x)"],
-      setupFiles: ["./src/__tests__/setupTests.ts"],
-      reporters: [
-        "default",
-        ...(process.env.GITHUB_ACTIONS ? [new GithubActionsReporter()] : []),
-      ], // prettier-ignore
+      setupFiles: ["./src/tests/setupTests.ts"],
+      reporters: ["default", ...(process.env.GITHUB_ACTIONS ? [new GithubActionsReporter()] : [])],
       coverage: {
+        include: ["src/**/*.{js,ts}"],
+        exclude: [...coverageConfigDefaults.exclude, "**/tests/**/*", "**/__mocks__/**/*"],
         reporter: [
-          // Default reporters:
-          "text",
-          "html",
-          "clover",
-          "json",
-          // Required for vitest-coverage-report GitHub Action:
-          "json-summary",
+          ...coverageConfigDefaults.reporter,
+          "json-summary", // <-- used by vitest-coverage-report GitHub Action
         ],
       },
     },
 
     build: {
-      outDir: "build",
+      target: "esnext",
+      outDir: "dist",
       sourcemap: true, // Required for Sentry-vite plugin
     },
   };
