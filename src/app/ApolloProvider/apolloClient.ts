377cef81d52459f331da7f13e86f6176a2306682
trevor-anderson <trevor@nerdware.cloud>
2024-03-06 05:21:25 -0500

    feat: enable Apollo dev tools in staging

--- src/app/ApolloProvider/apolloClient.ts
+++ src/app/ApolloProvider/apolloClient.ts
@@ -1,12 +1,12 @@
 import { ApolloClient } from "@apollo/client/core";
 import { ENV } from "@/app/env";
-import { apolloLink } from "./apolloLink";
 import { apolloCache } from "./apolloCache";
+import { apolloLink } from "./apolloLink";
 
 export const apolloClient = new ApolloClient({
   name: "fixit-web",
   ...(!!ENV?.PROJECT_VERSION && { version: ENV.PROJECT_VERSION }),
   cache: apolloCache,
   link: apolloLink,
-  connectToDevTools: ENV.IS_DEV,
+  connectToDevTools: !ENV.IS_PROD,
 });
