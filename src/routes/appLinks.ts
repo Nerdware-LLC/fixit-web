bd357bebeb942f1000adfc60252c8902a0e02551
trevor-anderson <trevor@nerdware.cloud>
2024-03-13 12:48:22 -0400

    feat: add APP_LINKS object w github repo url

---
+++ src/routes/appLinks.ts
@@ -0,0 +1,6 @@
+/**
+ * An object containing commonly-used external links.
+ */
+export const APP_LINKS = {
+  REPO: "https://github.com/Nerdware-LLC/fixit-web#readme",
+} as const satisfies Record<string, string>;
