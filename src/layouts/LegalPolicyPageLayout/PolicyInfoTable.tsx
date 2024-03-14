5684609a314c5181db92f1ccf2d870237d44ef11
trevor-anderson <trevor@nerdware.cloud>
2024-03-14 09:37:15 -0400

    feat: add PolicyInfoTable comp

---
+++ src/layouts/LegalPolicyPageLayout/PolicyInfoTable.tsx
@@ -0,0 +1,40 @@
+import { isString } from "@nerdware/ts-type-safety-utils";
+import { styled } from "@mui/material/styles";
+
+/**
+ * A simple table for displaying policy information.
+ */
+export const PolicyInfoTable = ({ headers, dataRows, ...tableProps }: PolicyInfoTableProps) => (
+  <StyledTable {...tableProps}>
+    <thead>
+      <tr>
+        {headers.map((header, index) => (
+          <th key={isString(header) ? header : index}>{header}</th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {dataRows.map((row, index) => (
+        <tr key={isString(row[0]) ? row[0] : index}>
+          {row.map((cell, index) => (
+            <td key={isString(cell) ? cell : index}>{cell}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </StyledTable>
+);
+
+export type PolicyInfoTableProps = {
+  headers: Array<React.ReactNode>;
+  dataRows: Array<Array<React.ReactNode>>;
+} & Omit<React.ComponentProps<typeof StyledTable>, "children">;
+
+const StyledTable = styled("table")(({ theme: { palette } }) => ({
+  borderCollapse: "collapse",
+
+  "& th,td": {
+    padding: "0.75rem",
+    border: `1px solid ${palette.grey[700]}`,
+  },
+}));
