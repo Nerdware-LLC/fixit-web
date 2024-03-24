9ad45cd40a56a5681aa50621b889aa2e783cff6d
trevor-anderson <trevor@nerdware.cloud>
2024-03-24 11:08:33 -0400

    feat: add root-app loading indicator to AppBarLogoButton

--- src/components/AppBar/AppBarLogoButton.tsx
+++ src/components/AppBar/AppBarLogoButton.tsx
@@ -1,5 +1,7 @@
 import { styled } from "@mui/material/styles";
+import CircularProgress, { circularProgressClasses } from "@mui/material/CircularProgress";
 import Text, { typographyClasses } from "@mui/material/Typography";
+import { useFetchStateContext } from "@/app/FetchStateContext";
 import { Logo, brandingClassNames } from "@/components/Branding";
 import { Anchor } from "@/components/Navigation/Anchor";
 import { APP_PATHS } from "@/routes/appPaths";
@@ -7,12 +9,16 @@ import { isAuthenticatedStore } from "@/stores";
 
 export const AppBarLogoButton = () => {
   const isAuthenticated = isAuthenticatedStore.useSubToStore();
+  const { isLoading } = useFetchStateContext();
 
   return (
-    <StyledDiv>
+    <StyledDiv className={brandingClassNames.titleLogoRoot}>
       <Anchor href={isAuthenticated ? APP_PATHS.HOME : APP_PATHS.ROOT}>
-        <Logo />
-        <Text variant="h1">Fixit</Text>
+        {isLoading && <CircularProgress />}
+        <Logo className={brandingClassNames.titleLogoImg} />
+        <Text variant="h1" className={brandingClassNames.titleLogoText}>
+          Fixit
+        </Text>
       </Anchor>
     </StyledDiv>
   );
@@ -24,9 +30,35 @@ const StyledDiv = styled("div")(({ theme: { palette, variables } }) => ({
   alignItems: "center",
 
   "& > a": {
-    // Increase the anchor's default hover-opacity (0.7)
+    position: "relative",
+
+    // Increase the anchor's default hover-opacity from 0.7 to 0.75
     "&:hover": {
-      opacity: 0.75,
+      // Why do the css selection like this? To avoid dimming the root app loading indicator.
+      [`& > .${brandingClassNames.fixitLogoImg},.${typographyClasses.root}`]: {
+        opacity: 0.75,
+      },
+    },
+
+    // ROOT APP LOADING INDICATOR
+    // Note: This is only shown if `isLoading` prop is set true in the top-most FetchStateContext.
+    [`& > .${circularProgressClasses.root}`]: {
+      position: "absolute",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      // These values ensure the spinner appears to go around the Logo img:
+      ...(variables.isMobilePageLayout
+        ? { height: "90% !important", left: "2px" }
+        : { height: "100% !important", left: "-2px" }),
+      [`& > svg.${circularProgressClasses.svg}`]: {
+        overflow: "visible",
+        // Like the parent el's height+left props, this height centers the spinner:
+        height: variables.isMobilePageLayout ? "130%" : "120%",
+        "& > circle": {
+          strokeWidth: variables.isMobilePageLayout ? "4px" : "5px",
+        },
+      },
     },
 
     [`& > .${brandingClassNames.fixitLogoImg}`]: {
