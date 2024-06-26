6fdbaf9e97de653a2e0af5eb9603f7d3199f4203
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-26 11:54:44 -0400

    refactor: mv normalizers into formatters, add i18n

--- src/utils/normalizers/strings.test.ts
+++
@@ -1,7 +0,0 @@
-import { normalizePhone } from "./strings";
-
-describe("normalizers/normalizePhone()", () => {
-  test("removes all non-numeric characters from the input", () => {
-    expect(normalizePhone("  (888) 123-4567  ")).toBe("8881234567");
-  });
-});
