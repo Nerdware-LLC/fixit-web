f35820ea5fc0de58baf3828a1f75d5e99de25317
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-06-26 12:16:41 -0400

    style: impl Mui Stack for simpler styling

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -16,28 +16,24 @@ import { RegisterForm } from "./RegisterForm.jsx";
 export const RegisterPage = () => (
   <AuthPageLayout pageTitle="User Registration">
     <RegisterForm />
-    <Text
-      variant="caption"
-      sx={{
-        whiteSpace: "pre-line",
-        "& > a": { display: "contents" },
-      }}
-    >
-      By registering your account, you agree to the Fixit{" "}
-      <Link to={APP_PATHS.ToS}>Terms of Service</Link> and the{" "}
-      <Anchor href="https://stripe.com/connect-account/legal/full">
-        Stripe Connected Account Agreement
-      </Anchor>
-      .
-    </Text>
-    <LegalLinks
-      style={{
-        /* This comp is nudged to the right by the below margin in order to
-        have the center of the form/page align with the center of the middle
-        word, rather than the center of the container (looks better). */
-        marginLeft: "1.25rem",
-      }}
-    />
+    <Stack spacing={2}>
+      <Text variant="caption" sx={{ "& > a": { display: "contents" } }}>
+        By registering your account, you agree to the Fixit{" "}
+        <Link to={APP_PATHS.ToS}>Terms of Service</Link> and the{" "}
+        <Anchor href={APP_URLS.STRIPE_CONNECTED_ACCOUNT_AGREEMENT}>
+          Stripe Connected Account Agreement
+        </Anchor>
+        .
+      </Text>
+      <LegalLinks
+        style={{
+          /* This comp is nudged to the right by the below margin in order to
+          have the center of the form/page align with the center of the middle
+          word, rather than the center of the container (looks better). */
+          marginLeft: "1.25rem",
+        }}
+      />
+    </Stack>
   </AuthPageLayout>
 );
 
