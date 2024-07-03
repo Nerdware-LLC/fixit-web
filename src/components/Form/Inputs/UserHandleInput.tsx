98dc2e6a77928530d09c514f3b60984303c6e517
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-07-03 09:01:16 -0400

    refactor: add autocomplete "name nickname"

--- src/components/Form/Inputs/UserHandleInput.tsx
+++ src/components/Form/Inputs/UserHandleInput.tsx
@@ -3,15 +3,21 @@ import InputAdornment from "@mui/material/InputAdornment";
 import { BaseTextField } from "./BaseTextField.jsx";
 import { formClassNames } from "../classNames.js";
 import { useFormikFieldProps } from "../helpers";
+import type { OverrideProperties } from "type-fest";
 import type { TextInputProps } from "./TextInput.jsx";
+import type { NameAutoCompleteValue } from "./types.js";
 
-export type UserHandleInputProps = TextInputProps;
+export type UserHandleInputProps = OverrideProperties<
+  TextInputProps,
+  { autoComplete?: NameAutoCompleteValue }
+>;
 
 export const UserHandleInput = <ValueType extends string | null | undefined = string>({
   id,
   label: explicitLabel,
   variant: explicitVariant,
   placeholder = "my_username",
+  autoComplete = "name nickname",
   ...userHandleInputProps
 }: UserHandleInputProps) => {
   const [{ value, ...textInputProps }, { setValue }] = useFormikFieldProps<ValueType>({
@@ -35,13 +41,15 @@ export const UserHandleInput = <ValueType extends string | null | undefined = st
 
   return (
     <BaseTextField
-      className={formClassNames.textInput}
       value={value ?? ""}
+      autoComplete={autoComplete}
+      className={formClassNames.textInput}
       {...textInputProps}
       {...userHandleInputProps}
       InputProps={{
         ...(userHandleInputProps.InputProps ?? {}),
         startAdornment: <InputAdornment position="start">@</InputAdornment>,
+        autoComplete,
       }}
     />
   );
