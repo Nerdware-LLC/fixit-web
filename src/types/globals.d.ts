e0a72207de9995fc29536732f079c3c89388985c
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-07-03 07:05:54 -0400

    build(types): rm unnecessary JSON.parse overloads

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -3,26 +3,18 @@ import type { GoogleOAuthClient } from "./googleOAuth.js";
 
 declare global {
   /**
-   * This declaration makes the following modifications to the `JSON.parse` typedef:
-   *
-   * - For the `string` argument overload, the `any` return type is replaced with
-   *   {@link JsonValue | type-fest's `JsonValue`} .
-   * - Add `number` overload, since `JSON.parse(42)` is valid and returns `42`.
-   * - Add `null` overload, since `JSON.parse(null)` is valid and returns `null`.
+   * This declaration adds `JSON.parse` overload that replaces `any` with {@link JsonValue}.
    */
   interface JSON {
     parse(
-      text: string | null,
-      reviver?: (this: any, key: string, value: unknown) => unknown
+      text: JsonValue,
+      reviver?: (this: typeof JSON, key: string, value: unknown) => unknown
     ): JsonValue;
-    parse(text: number, reviver?: (this: any, key: string, value: unknown) => unknown): number;
-    parse(text: null, reviver?: (this: any, key: string, value: unknown) => unknown): null;
   }
 
   interface Window {
     /**
-     * Google OAuth Client
-     * > The Google OAuth client library is loaded asynchronously via a script tag.
+     * The Google OAuth Client library is loaded asynchronously via a script tag.
      */
     google?: GoogleOAuthClient;
   }
