ea133efd4d5ba5155defed2908262610060c684e
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-07-03 07:31:23 -0400

    feat: add minor input sanitization
    UserHandleInput now no longer allows users to input `@` chars or spaces, which simplifies input.

--- src/components/Form/Inputs/UserHandleInput.tsx
+++ src/components/Form/Inputs/UserHandleInput.tsx
@@ -1,14 +1,48 @@
+import { useEffect } from "react";
 import InputAdornment from "@mui/material/InputAdornment";
-import { TextInput, type TextInputProps } from "./TextInput.jsx";
-
-export const UserHandleInput = ({ ...userHandleInputProps }: UserHandleInputProps) => (
-  <TextInput
-    {...userHandleInputProps}
-    InputProps={{
-      ...(userHandleInputProps.InputProps ?? {}),
-      startAdornment: <InputAdornment position="start">@</InputAdornment>,
-    }}
-  />
-);
+import { BaseTextField } from "./BaseTextField.jsx";
+import { formClassNames } from "../classNames.js";
+import { useFormikFieldProps } from "../helpers";
+import type { TextInputProps } from "./TextInput.jsx";
 
 export type UserHandleInputProps = TextInputProps;
+
+export const UserHandleInput = <ValueType extends string | null | undefined = string>({
+  id,
+  label: explicitLabel,
+  variant: explicitVariant,
+  placeholder = "my_username",
+  ...userHandleInputProps
+}: UserHandleInputProps) => {
+  const [{ value, ...textInputProps }, { setValue }] = useFormikFieldProps<ValueType>({
+    fieldID: id,
+    label: explicitLabel,
+    variant: explicitVariant,
+    placeholder,
+  });
+
+  // EFFECT: Remove @ and any whitespace from the user handle `value` to simplify user input
+  useEffect(() => {
+    (async () => {
+      // If `value` is falsey, do nothing
+      if (!value) return;
+
+      const formattedValue = value.replace(/(@|\s)/g, "");
+
+      if (value !== formattedValue) await setValue(formattedValue as ValueType);
+    })();
+  }, [value, setValue]);
+
+  return (
+    <BaseTextField
+      className={formClassNames.textInput}
+      value={value ?? ""}
+      {...textInputProps}
+      {...userHandleInputProps}
+      InputProps={{
+        ...(userHandleInputProps.InputProps ?? {}),
+        startAdornment: <InputAdornment position="start">@</InputAdornment>,
+      }}
+    />
+  );
+};
