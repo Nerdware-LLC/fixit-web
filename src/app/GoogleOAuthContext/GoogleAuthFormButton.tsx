c5ef1238d4d24b4651304b35370350d195e69794
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-07-03 07:14:47 -0400

    refactor: rm props `requiredFieldInputs` and `beforeSetFormikState`
    These props were made unnecessary by the flow-changes to RegisterForm.

--- src/app/GoogleOAuthContext/GoogleAuthFormButton.tsx
+++ src/app/GoogleOAuthContext/GoogleAuthFormButton.tsx
@@ -1,13 +1,7 @@
 import { useState, useEffect } from "react";
 import { useFormikContext, type FormikState } from "formik";
 import { jwtDecode } from "jwt-decode";
-import DialogTitle from "@mui/material/DialogTitle";
-import Text from "@mui/material/Typography";
-import GoogleIcon from "@mui/icons-material/Google";
 import { useFetchStateContext } from "@/app/FetchStateContext/useFetchStateContext.js";
-import { globalAnimations } from "@/app/GlobalStyles/animations.js";
-import { CloseIconButton } from "@/components/Buttons/CloseIconButton.jsx";
-import { Dialog } from "@/components/Dialog";
 import { GoogleOAuthButton, type GoogleOAuthButtonProps } from "./GoogleOAuthButton.jsx";
 import type { CredentialResponse } from "@/types/googleOAuth.js";
 import type { Except, OverrideProperties } from "type-fest";
@@ -25,57 +19,31 @@ type FormikValuesErrorsTouched<AuthFormValues extends BaseAuthFormValues> = Pick
   "values" | "errors" | "touched"
 >;
 
-export type GoogleAuthFormButtonProps<AuthFormValues extends BaseAuthFormValues> =
-  OverrideProperties<
-    Except<GoogleOAuthButtonProps, "onSuccess" | "onError">,
-    {
-      text: Extract<GoogleOAuthButtonProps["text"], "signin_with" | "signup_with">;
-    }
-  > & {
-    /**
-     * Required-field input elements to be displayed in the "required-fields" Dialog if their
-     * respective values are not present in the Form context upon the User's completion of the
-     * Google OAuth prompt flow.
-     */
-    requiredFieldInputs: React.ReactNode;
-    /**
-     * A hook for transforming the Formik state object. After the User completes the Google OAuth
-     * prompt flow, this component makes a best effort to ascertain desired values for Formik state
-     * objects — `values`, `errors`, and `touched` — using a combination of the Google OAuth2 ID
-     * token and current Form state. It then passes these values to `beforeSetFormikState` to allow
-     * the User to configure the Formik state as desired.
-     * > Use this function to handle state updates for form fields this component does not handle.
-     */
-    beforeSetFormikState?: (
-      formikState: FormikValuesErrorsTouched<AuthFormValues>
-    ) => FormikValuesErrorsTouched<AuthFormValues>;
-  };
+export type GoogleAuthFormButtonProps = OverrideProperties<
+  Except<GoogleOAuthButtonProps, "onSuccess" | "onError">,
+  {
+    text: Extract<GoogleOAuthButtonProps["text"], "signin_with" | "signup_with">;
+  }
+>;
 
 /**
  * A button that prompts the User to sign in or sign up with Google OAuth2. When the User
  * completes the Google OAuth prompt flow, this component makes a best effort to ascertain
  * desired values for Formik state objects — `values`, `errors`, and `touched` — for the
  * following common auth-form fields:
- *
- *   - `email`
- *   - `googleIDToken`
- *   - `password`
- *
- * Other form fields are supported via hook props like `beforeSetFormikState`.
+ * - `email`
+ * - `googleIDToken`
+ * - `password`
  */
 export const GoogleAuthFormButton = <AuthFormValues extends BaseAuthFormValues>({
   text, // "signin_with" or "signup_with"
-  requiredFieldInputs,
-  beforeSetFormikState,
   ...googleOAuthButtonProps
-}: GoogleAuthFormButtonProps<AuthFormValues>) => {
+}: GoogleAuthFormButtonProps) => {
   const formikContext = useFormikContext<AuthFormValues>();
   const { values, errors, touched } = formikContext;
-  const { setValues, setErrors, setTouched, validateForm, handleSubmit } = formikContext;
+  const { setValues, setErrors, setTouched, handleSubmit } = formikContext;
 
   const { setError: setFetchError } = useFetchStateContext();
-  const { isDialogVisible, openDialog, closeDialog } = Dialog.use();
-  const [shouldWiggle, setShouldWiggle] = useState(false);
 
   // See below remarks regarding the Formik stale-values bug and workaround.
   // TODO Remove this state once Formik PR is merged
@@ -118,7 +86,7 @@ export const GoogleAuthFormButton = <AuthFormValues extends BaseAuthFormValues>(
     if (!newEmailValue) {
       try {
         const payload = jwtDecode<{ email?: string }>(googleIDToken);
-        if (payload?.email) newEmailValue = payload.email;
+        if (payload.email) newEmailValue = payload.email;
       } catch {
         handleError();
         return;
@@ -127,7 +95,7 @@ export const GoogleAuthFormButton = <AuthFormValues extends BaseAuthFormValues>(
 
     // Pass the Formik state to the `beforeSetFormikState` function to allow for any additional
     // state updates that are not handled by the `GoogleAuthFormButton` component itself.
-    let newFormikState: FormikValuesErrorsTouched<AuthFormValues> = {
+    const newFormikState: FormikValuesErrorsTouched<AuthFormValues> = {
       values: {
         ...values,
         googleIDToken,
@@ -148,80 +116,22 @@ export const GoogleAuthFormButton = <AuthFormValues extends BaseAuthFormValues>(
       },
     };
 
-    // If `beforeSetFormikState` was provided, use it to update the Formik state object
-    if (beforeSetFormikState) newFormikState = beforeSetFormikState(newFormikState);
-
     // Perform all state updates at once via `setFormikState`
     await setValues(newFormikState.values, true);
     await setTouched(newFormikState.touched, false);
     setErrors(newFormikState.errors);
 
-    // If the new state lists any errors, open the "required-fields" Dialog, else submit the form
-    if (Object.values(newFormikState.errors).some(Boolean)) openDialog();
-    else setShouldSubmit(true); // TODO <-- replace this with `handleSubmit` once Formik PR is merged
+    // If the new state does not contain errors, submit the form
+    if (!Object.values(newFormikState.errors).some(Boolean)) setShouldSubmit(true);
+    // TODO Replace above `setShouldSubmit` with `handleSubmit` once Formik PR is merged
   };
-
-  /** When the User clicks "OK", validate the form and close the dialog if there are no errors. */
-  const handleAcceptDialog = async () => {
-    await validateForm();
-
-    if (Object.values(errors).some(Boolean)) {
-      setShouldWiggle(true);
-      setTimeout(() => setShouldWiggle(false), 1000);
-    } else {
-      handleCloseDialog();
-    }
-  };
-
-  /** Close dialog, submit form. */
-  const handleCloseDialog = () => {
-    closeDialog();
-    handleSubmit();
-  };
-
-  const { dialogTitle, promptMessage } =
-    text === "signup_with"
-      ? {
-          dialogTitle: "Sign Up with Google",
-          promptMessage: "Please provide these required fields to complete your registration:",
-        }
-      : {
-          dialogTitle: "Sign In with Google",
-          promptMessage: "Please provide these required fields to continue:",
-        };
 
   return (
-    <>
-      <GoogleOAuthButton
-        text={text} // "signin_with" or "signup_with"
-        onSuccess={handleSuccess}
-        onError={handleError}
-        {...googleOAuthButtonProps}
-      />
-      <Dialog
-        title={
-          <DialogTitle
-            color="secondary"
-            style={{ display: "flex", alignItems: "center", gap: "0.6rem" }}
-          >
-            <GoogleIcon /> {dialogTitle}
-            <CloseIconButton onClick={closeDialog} />
-          </DialogTitle>
-        }
-        isVisible={isDialogVisible}
-        handleAccept={handleAcceptDialog}
-        onClose={handleCloseDialog}
-        PaperProps={{ className: shouldWiggle ? globalAnimations.wiggleX.className : undefined }}
-        dialogContentProps={{
-          sx: {
-            padding: "1.5rem 1.5rem 0.5rem 1.5rem",
-            gap: "0.5rem !important",
-          },
-        }}
-      >
-        <Text style={{ marginBottom: "1rem" }}>{promptMessage}</Text>
-        {requiredFieldInputs}
-      </Dialog>
-    </>
+    <GoogleOAuthButton
+      text={text} // "signin_with" or "signup_with"
+      onSuccess={handleSuccess}
+      onError={handleError}
+      {...googleOAuthButtonProps}
+    />
   );
 };
