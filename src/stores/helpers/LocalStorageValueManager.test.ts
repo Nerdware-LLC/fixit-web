6f780f389dfaaf732c2be24ee53349b083f89904
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-07-03 06:55:41 -0400

    feat: add TTL to LSVM

--- src/stores/helpers/LocalStorageValueManager.test.ts
+++ src/stores/helpers/LocalStorageValueManager.test.ts
@@ -6,15 +6,17 @@ describe("LocalStorageValueManager", () => {
 
   describe("new LocalStorageValueManager()", () => {
     test(`returns instanceof LocalStorageValueManager when invoked with valid arguments`, () => {
-      expect(new LocalStorageValueManager(MOCK_KEY, "")).toBeInstanceOf(LocalStorageValueManager);
+      expect(new LocalStorageValueManager(MOCK_KEY, { initialValue: "" })).toBeInstanceOf(
+        LocalStorageValueManager
+      );
     });
     test("sets the initialValue argument value in localStorage upon instantiation", () => {
       // Arrange initial value input
       const initialValue = "initialValue";
       // Act
-      new LocalStorageValueManager(MOCK_KEY, initialValue);
+      new LocalStorageValueManager(MOCK_KEY, { initialValue });
       // Assert
-      expect(localStorage.getItem(MOCK_KEY)).toBe(`"${initialValue}"`);
+      expect(localStorage.getItem(MOCK_KEY)).toBe(`{"data":"${initialValue}"}`);
     });
     test("does not set a value in localStorage when creating an instance without an initialValue argument", () => {
       // Act
@@ -27,23 +29,32 @@ describe("LocalStorageValueManager", () => {
   describe("get()", () => {
     test("returns the parsed value from localStorage when retrieving a pre-existing stored value", () => {
       // Arrange pre-existing value in localStorage
-      const preExistingValue = "preExistingValue";
-      localStorage.setItem(MOCK_KEY, preExistingValue);
+      localStorage.setItem(MOCK_KEY, `{"data":"preExistingValue"}`);
       // Arrange instance
       const manager = new LocalStorageValueManager(MOCK_KEY); // <-- no initial value
       // Act
       const retrievedValue = manager.get();
       // Assert
-      expect(retrievedValue).toBe(preExistingValue);
+      expect(retrievedValue).toBe("preExistingValue");
     });
     test("returns the parsed value from localStorage when retrieving an initially stored value", () => {
       // Arrange instance with initial value
       const initialValue = "initialValue";
-      const manager = new LocalStorageValueManager(MOCK_KEY, initialValue); // <-- initial value
+      const manager = new LocalStorageValueManager(MOCK_KEY, { initialValue }); // <-- initial value
       // Act
       const retrievedValue = manager.get();
       // Assert
       expect(retrievedValue).toBe(initialValue);
+    });
+    test("returns null when retrieving an expired value", () => {
+      // Assert using an LSVM with an instance `ttl` property
+      const manager_withTTL = new LocalStorageValueManager(MOCK_KEY, { initialValue: "", ttl: -1 });
+      expect(manager_withTTL.get()).toBe(null);
+
+      // Assert using an LSVM with no `ttl`, but ttl is set via the `set()` method
+      const manager_noTTL = new LocalStorageValueManager(MOCK_KEY);
+      manager_noTTL.set("", -1);
+      expect(manager_noTTL.get()).toBe(null);
     });
     test("returns the parsed value from localStorage when retrieving a value stored by the set() method", () => {
       // Arrange instance that stores a value via .set()
@@ -61,20 +72,20 @@ describe("LocalStorageValueManager", () => {
     test("creates a stored value in localStorage at the provided key", () => {
       // Arrange instance with NO initial value
       const manager = new LocalStorageValueManager<string>(MOCK_KEY);
-      const newValue = "newValue";
       // Act
-      manager.set(newValue);
+      manager.set("newValue");
       // Assert
-      expect(localStorage.getItem(MOCK_KEY)).toBe(`"${newValue}"`);
+      expect(localStorage.getItem(MOCK_KEY)).toBe(`{"data":"newValue"}`);
     });
     test("updates the stored value in localStorage at the provided key", () => {
       // Arrange instance WITH initial value
-      const manager = new LocalStorageValueManager<string>(MOCK_KEY, "initialValue");
-      const newValue = "newValue";
+      const manager = new LocalStorageValueManager<string>(MOCK_KEY, {
+        initialValue: "initialValue",
+      });
       // Act
-      manager.set(newValue);
+      manager.set("newValue");
       // Assert
-      expect(localStorage.getItem(MOCK_KEY)).toBe(`"${newValue}"`);
+      expect(localStorage.getItem(MOCK_KEY)).toBe(`{"data":"newValue"}`);
     });
   });
 
@@ -82,27 +93,29 @@ describe("LocalStorageValueManager", () => {
     test("creates a stored value in localStorage at the provided key if no value yet exists", () => {
       // Arrange instance
       const manager = new LocalStorageValueManager(MOCK_KEY);
-      const newValue = "newValue";
-      // Act
-      manager.setDefaultIfEmpty(newValue);
-      // Assert
-      expect(localStorage.getItem(MOCK_KEY)).toBe(`"${newValue}"`);
-    });
-    test("does not update localStorage at the provided key if a value already exists", () => {
-      // Arrange instance with an initial value
-      const initialValue = "initialValue";
-      const manager = new LocalStorageValueManager<string>(MOCK_KEY, initialValue);
       // Act
       manager.setDefaultIfEmpty("newValue");
       // Assert
-      expect(localStorage.getItem(MOCK_KEY)).toBe(`"${initialValue}"`);
+      expect(localStorage.getItem(MOCK_KEY)).toBe(`{"data":"newValue"}`);
+    });
+    test("does not update localStorage at the provided key if a value already exists", () => {
+      // Arrange instance with an initial value
+      const manager = new LocalStorageValueManager<string>(MOCK_KEY, {
+        initialValue: "initialValue",
+      });
+      // Act
+      manager.setDefaultIfEmpty("newValue");
+      // Assert
+      expect(localStorage.getItem(MOCK_KEY)).toBe(`{"data":"initialValue"}`);
     });
   });
 
   describe("remove()", () => {
     test("removes the stored value from localStorage at the provided key", () => {
       // Arrange instance with an initial value
-      const manager = new LocalStorageValueManager<string>(MOCK_KEY, "initialValue");
+      const manager = new LocalStorageValueManager<string>(MOCK_KEY, {
+        initialValue: "initialValue",
+      });
       // Act
       manager.remove();
       // Assert
--- src/stores/helpers/LocalStorageValueManager.ts
+++ src/stores/helpers/LocalStorageValueManager.ts
@@ -22,11 +22,14 @@ import type { Jsonifiable } from "type-fest";
  * manager.get(); //    Returns "default"
  * ```
  */
-export class LocalStorageValueManager<StoredType extends Jsonifiable> {
+export class LocalStorageValueManager<StoredValueType extends Jsonifiable> {
   protected readonly storageKey: string;
+  /** A default TTL to apply to all data stored by an LSVM instance. */
+  protected readonly ttl: StoredValueTTL;
 
-  constructor(key: string, initialValue?: StoredType) {
+  constructor(key: string, { initialValue, ttl }: LsvmOptions<StoredValueType> = {}) {
     this.storageKey = key;
+    this.ttl = ttl;
     if (initialValue !== undefined) this.setDefaultIfEmpty(initialValue);
   }
 
@@ -34,39 +37,77 @@ export class LocalStorageValueManager<StoredType extends Jsonifiable> {
    * Retrieves the stored value from `localStorage` and parses it from JSON.
    * @returns The stored value, parsed from JSON, or `null` if the stored value is empty.
    */
-  get() {
-    const storedValue = localStorage.getItem(this.storageKey);
-    let returnValue;
+  readonly get = (): StoredValueType | null => {
+    const rawValue = localStorage.getItem(this.storageKey);
+
+    if (rawValue === null) return null;
+
+    let lsvmDataWrapper;
+
     try {
-      returnValue = JSON.parse(storedValue);
+      lsvmDataWrapper = JSON.parse(rawValue) as unknown as LsvmDataWrapper<StoredValueType> | null;
+      if (!lsvmDataWrapper) return null;
     } catch {
-      returnValue = storedValue;
+      return null;
     }
-    return returnValue as StoredType;
-  }
+
+    if (!!lsvmDataWrapper.expiresAt && lsvmDataWrapper.expiresAt < Date.now()) {
+      this.remove();
+      return null;
+    }
+
+    return lsvmDataWrapper.data ?? null;
+  };
 
   /**
    * Stores a new value as stringified JSON in `localStorage`.
    * @param value - The value to be stored.
    */
-  set(value: StoredType): void {
-    const jsonValue = safeJsonStringify(value);
-    localStorage.setItem(this.storageKey, jsonValue);
-  }
+  readonly set = (value: StoredValueType, ttl: StoredValueTTL = this.ttl): void => {
+    const valueDataWrapper: LsvmDataWrapper<StoredValueType> = {
+      data: value,
+      ...(ttl && { expiresAt: Date.now() + ttl }),
+    };
+
+    localStorage.setItem(this.storageKey, safeJsonStringify(valueDataWrapper));
+  };
 
   /**
    * Stores a default value in `localStorage` if the stored value is empty.
    * @param defaultValue - The value to be stored if the stored value is empty.
    */
-  setDefaultIfEmpty(defaultValue: StoredType): void {
+  readonly setDefaultIfEmpty = (defaultValue: StoredValueType): void => {
     const currentlyStoredValue = this.get();
     if (currentlyStoredValue === null) this.set(defaultValue);
-  }
+  };
 
   /**
    * Removes the stored value from `localStorage`.
    */
-  remove(): void {
+  readonly remove = (): void => {
     localStorage.removeItem(this.storageKey);
-  }
+  };
+}
+
+/**
+ * Options that can be provided to the LSVM constructor.
+ */
+type LsvmOptions<StoredValueType extends Jsonifiable> = {
+  initialValue?: StoredValueType | undefined;
+  ttl?: StoredValueTTL;
+};
+
+/**
+ * The time-to-live (TTL) value to apply to stored value.
+ */
+type StoredValueTTL = number | undefined;
+
+/**
+ * The JSON wrapper object LSVM uses to manage data in LocalStorage.
+ */
+interface LsvmDataWrapper<StoredValueType extends Jsonifiable> {
+  /** The value stored in LocalStorage. */
+  data: StoredValueType;
+  /** Timestamp reflecting when the data expires. */
+  expiresAt?: StoredValueTTL;
 }
