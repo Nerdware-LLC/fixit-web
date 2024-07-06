96fc9888edd830fb8ac56708c617d6c8038f67a1
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-07-06 07:34:15 -0400

    feat(apollo): enable assumeImmutableResults

--- src/app/ApolloProvider/apolloClient.ts
+++ src/app/ApolloProvider/apolloClient.ts
@@ -9,5 +9,5 @@ export const apolloClient = new ApolloClient({
   cache: apolloCache,
   link: apolloLink,
   connectToDevTools: !ENV.IS_PROD,
-  // TODO - experiment with setting `assumeImmutableResults` to `true` (apollo says it's faster)
+  assumeImmutableResults: true, // enables substantial performance optimizations
 });
