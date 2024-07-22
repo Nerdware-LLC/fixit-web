628d77c6c13322c78eeefd8837f863f29b254325
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-07-22 10:56:17 -0400

    refactor: add LPPL classes for greater spec of key styles

--- src/layouts/LegalPolicyPageLayout/LegalPolicyPageLayout.tsx
+++ src/layouts/LegalPolicyPageLayout/LegalPolicyPageLayout.tsx
@@ -1,42 +1,50 @@
 import { styled } from "@mui/material/styles";
 import Text, { typographyClasses } from "@mui/material/Typography";
+import { LegalLinksFooter } from "@/components/Navigation";
+import { legalPolicyPageLayoutClassNames } from "./classNames.js";
 
 /**
- * Layout used by `PrivacyPolicyPage` and `TermsOfServicePage`.
+ * Layout used by pages like `PrivacyPolicyPage` and `TermsOfServicePage`.
  */
 export const LegalPolicyPageLayout = ({
   pageTitle,
   lastUpdated,
   children,
 }: LegalPolicyPageLayoutProps) => (
-  <StyledDiv>
-    <Text variant="h2" component="h1">
-      {pageTitle}
-    </Text>
-    <Text
-      variant="h6"
-      component="h2"
-      style={{
-        marginTop: "0.5rem",
-        transform: "translateX(0.25rem)", // better alignment with above+below headers
-        fontWeight: "lighter",
-      }}
-    >
-      Last updated {lastUpdated}
-    </Text>
-    {children}
+  <StyledDiv className={legalPolicyPageLayoutClassNames.root}>
+    <div className={legalPolicyPageLayoutClassNames.contentRoot}>
+      <Text variant="h2" component="h1">
+        {pageTitle}
+      </Text>
+      <Text
+        variant="h6"
+        component="h2"
+        style={{
+          marginTop: "0.5rem",
+          transform: "translateX(0.25rem)", // better alignment with above+below headers
+          fontWeight: "lighter",
+        }}
+      >
+        Last updated {lastUpdated}
+      </Text>
+      {children}
+    </div>
+    <LegalLinksFooter />
   </StyledDiv>
 );
 
 const StyledDiv = styled("div")(({ theme: { breakpoints } }) => ({
-  minHeight: "100%",
-  maxWidth: "1000px",
-  margin: "0 auto",
-  padding: "1.5rem",
-  [breakpoints.up("sm")]: { padding: "2rem" },
-
   [`& .${typographyClasses.body1}`]: {
     margin: "1rem 0",
+  },
+
+  [`& > .${legalPolicyPageLayoutClassNames.contentRoot}`]: {
+    minHeight: "100%",
+    maxWidth: "1000px",
+    margin: "0 auto",
+    padding: "1.5rem",
+    // sm = 600px
+    [breakpoints.up("sm")]: { padding: "2rem" },
   },
 }));
 
---
+++ src/layouts/LegalPolicyPageLayout/classNames.ts
@@ -0,0 +1,7 @@
+/**
+ * Class names for `LegalPolicyPageLayout` components.
+ */
+export const legalPolicyPageLayoutClassNames = {
+  root: "legal-policy-page-layout__root",
+  contentRoot: "legal-policy-page-layout__content-root",
+} as const;
