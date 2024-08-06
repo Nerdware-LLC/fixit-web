d6aebb240ddc0c804b3ec737fd57ab2a6082edea
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-08-06 05:04:55 -0400

    refactor: improve styles for better layout

--- src/components/DataDisplay/InvoiceWorkOrderDetails.tsx
+++ src/components/DataDisplay/InvoiceWorkOrderDetails.tsx
@@ -1,15 +1,19 @@
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { styled } from "@mui/material/styles";
 import ToolsIcon from "@mui/icons-material/Construction";
-import { usePageLayoutContext } from "@/app/PageLayoutContext/usePageLayoutContext.js";
 import { WorkOrderCategoryChip } from "@/components/Chips/WorkOrderCategoryChip.jsx";
 import { WorkOrderStatusChip } from "@/components/Chips/WorkOrderStatusChip.jsx";
-import { LinkToWorkOrder } from "@/components/Navigation/LinkToWorkOrder.jsx";
+import { LinkToWorkOrder } from "@/components/Navigation";
 import { getDateStr } from "@/utils/formatters/dateTime.js";
 import { ItemDetails, type ItemDetailsProps } from "./ItemDetails.jsx";
 import { ItemDetailsGroup } from "./ItemDetailsGroup.jsx";
 import { LocationDetails } from "./LocationDetails.jsx";
 import { dataDisplayClassNames } from "./classNames.js";
 import type { Invoice } from "@/types/graphql.js";
+
+export type InvoiceWorkOrderDetailsProps = {
+  workOrder?: Invoice["workOrder"];
+  label?: string;
+} & Omit<ItemDetailsProps, "className">;
 
 /**
  * A component which displays info about an Invoice's WorkOrder on non-mobile
@@ -18,82 +22,72 @@ import type { Invoice } from "@/types/graphql.js";
 export const InvoiceWorkOrderDetails = ({
   workOrder,
   label = "Work Order",
-  sx,
   ...containerProps
-}: InvoiceWorkOrderDetailsProps) => {
-  const { isMobilePageLayout } = usePageLayoutContext();
+}: InvoiceWorkOrderDetailsProps) => (
+  <ItemDetailsGroup
+    label={label}
+    labelIcon={<ToolsIcon />}
+    headerComponents={
+      workOrder ? (
+        <LinkToWorkOrder workOrderID={workOrder.id} text="View" style={{ marginLeft: "auto" }} />
+      ) : null
+    }
+    className={dataDisplayClassNames.invoiceWorkOrderDetailsRoot}
+    {...containerProps}
+  >
+    <StyledDiv className={dataDisplayClassNames.invoiceWorkOrderDetailsContentWrapper}>
+      <LocationDetails location={workOrder?.location} gridArea="location" />
 
-  // prettier-ignore
-  const woLink = workOrder ? (
-    <LinkToWorkOrder workOrderID={workOrder.id}>
-      {!isMobilePageLayout ? <>View <ChevronRightIcon /></> : null}
-    </LinkToWorkOrder>
-  ) : null;
+      <ItemDetails label="Status" gridArea="status">
+        {workOrder?.status ? <WorkOrderStatusChip status={workOrder.status} /> : null}
+      </ItemDetails>
+      <ItemDetails label="Category" gridArea="category">
+        {workOrder?.category ? <WorkOrderCategoryChip category={workOrder.category} /> : null}
+      </ItemDetails>
 
-  return (
-    <>
-      {isMobilePageLayout ? (
-        <ItemDetails
-          label={label}
-          sx={sx}
-          className={dataDisplayClassNames.invoiceWorkOrderDetailsRoot}
-          {...containerProps}
-        >
-          {woLink}
-        </ItemDetails>
-      ) : (
-        <ItemDetailsGroup
-          label={label}
-          labelIcon={<ToolsIcon />}
-          headerComponents={woLink}
-          className={dataDisplayClassNames.invoiceWorkOrderDetailsRoot}
-          style={{
-            minWidth: "4rem",
-            display: "flex",
-            flexDirection: "column",
-            maxHeight: "100%",
-          }}
-          sx={{
-            [`& > .${dataDisplayClassNames.groupHeader}`]: {
-              [`& > .${dataDisplayClassNames.label}`]: {
-                marginRight: "auto",
-              },
-              // The "View" anchor link in the header:
-              "& a": {
-                marginLeft: "1rem", // ensure at least 1rem of space between the label and the anchor
-                display: "flex",
-                alignItems: "center",
-                textDecoration: "none",
-              },
-            },
-            [`& > .${dataDisplayClassNames.groupContent}`]: {
-              flexGrow: 1,
-              justifyContent: "space-between",
-            },
-            ...sx,
-          }}
-          {...containerProps}
-        >
-          <ItemDetails label="Status">
-            {workOrder?.status ? <WorkOrderStatusChip status={workOrder.status} /> : null}
-          </ItemDetails>
-          <LocationDetails location={workOrder?.location} />
-          <ItemDetails label="Category">
-            {workOrder?.category ? <WorkOrderCategoryChip category={workOrder.category} /> : null}
-          </ItemDetails>
-          <ItemDetails label="Created">
-            {workOrder ? getDateStr(workOrder.createdAt) : null}
-          </ItemDetails>
-          <ItemDetails label="Updated">
-            {workOrder ? getDateStr(workOrder.updatedAt) : null}
-          </ItemDetails>
-        </ItemDetailsGroup>
-      )}
-    </>
-  );
-};
+      <ItemDetails label="Created" gridArea="createdAt">
+        {workOrder ? getDateStr(workOrder.createdAt) : null}
+      </ItemDetails>
+      <ItemDetails label="Updated" gridArea="updatedAt">
+        {workOrder ? getDateStr(workOrder.updatedAt) : null}
+      </ItemDetails>
+    </StyledDiv>
+  </ItemDetailsGroup>
+);
 
-export type InvoiceWorkOrderDetailsProps = {
-  workOrder?: Invoice["workOrder"];
-  label?: string;
-} & Omit<ItemDetailsProps, "className">;
+const StyledDiv = styled("div")(({ theme: { variables } }) => ({
+  minHeight: "min-content",
+  height: "max-content",
+  width: "fit-content",
+  flexGrow: 1,
+
+  // LAYOUT
+  ...(variables.isMobilePageLayout
+    ? {
+        display: "grid",
+        gap: "2rem",
+        alignSelf: "center",
+        gridTemplateColumns: "repeat(2, 1fr)",
+        gridTemplateAreas: `
+          "location   location"
+          "status     category"
+          "createdAt  updatedAt"`,
+      }
+    : {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        gap: "inherit",
+      }),
+
+  // LOCATION DETAILS
+  [`& > .${dataDisplayClassNames.locationDetails}`]: {
+    minWidth: "max-content",
+    display: "flex",
+    flexDirection: "column",
+    [`& > .${dataDisplayClassNames.content}`]: {
+      flexGrow: 1,
+      justifyContent: "space-evenly",
+    },
+  },
+}));
