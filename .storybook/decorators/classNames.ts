f07639d5800084b33c242ffdc73c4108d852ca00
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-08-06 04:42:02 -0400

    refactor: replace StoryInfo classes w element-ids

--- .storybook/decorators/classNames.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Class names for Storybook decorator components.
- */
-export const sbDecoratorClassNames = {
-  storyInfo: "sb-decorator-story-info",
-} as const;
---
+++ .storybook/decorators/elementIDs.ts
@@ -0,0 +1,10 @@
+/**
+ * Element IDs for Storybook decorator components.
+ */
+export const sbDecoratorElementIDs = {
+  storyInfoRoot: "sb-decorator__story-info__root",
+  storyInfoPanelRoot: "sb-decorator__story-info__panel-root",
+  storyInfoDivider: "sb-decorator__story-info__divider",
+  storyInfoShowHideBtn: "sb-decorator__story-info__show-hide-btn",
+  storyInfoWrapper: "sb-decorator__story-info__wrapper",
+} as const;
