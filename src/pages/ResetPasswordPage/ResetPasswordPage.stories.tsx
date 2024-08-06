d191e95302e686d991aed9cafdb3f3364bf07c0a
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-08-06 05:16:49 -0400

    refactor: replace `withNavDecorator` w `withRootAppLayoutDecorator`

--- src/pages/ResetPasswordPage/ResetPasswordPage.stories.tsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.stories.tsx
@@ -1,15 +1,15 @@
-import { withNavDecorator, type NavDecoratorArgs } from "@/../.storybook/decorators";
+import { withRootAppLayoutDecorator, type LayoutDecoratorArgs } from "@/../.storybook/decorators";
 import { ResetPasswordPage } from "./ResetPasswordPage.jsx";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
   title: "Pages/ResetPasswordPage",
   component: ResetPasswordPage,
-  decorators: [withNavDecorator],
+  decorators: [withRootAppLayoutDecorator],
   parameters: {
     layout: "fullscreen",
   },
-} satisfies Meta<typeof ResetPasswordPage & NavDecoratorArgs>;
+} satisfies Meta<typeof ResetPasswordPage & LayoutDecoratorArgs>;
 
 export default meta;
 
@@ -20,8 +20,8 @@ type Story = StoryObj<typeof meta>;
 
 export const BasicDemo = {
   args: {
-    _nav_decorator_args: {
-      route: "/reset-password?token=1234567890abcdef",
+    _layout_decorator_args: {
+      initialEntries: ["/reset-password?token=1234567890abcdef"],
     },
   },
 } satisfies Story;
