fe6f3b23fcef60d51ae0da454dcd879359371e56
trevor-anderson <43518091+trevor-anderson@users.noreply.github.com>
2024-08-06 05:17:19 -0400

    feat(sb): add `apolloClient.writeQuery` calls for cache-only queries

--- src/pages/WorkOrderFormView/WorkOrderFormView.stories.tsx
+++ src/pages/WorkOrderFormView/WorkOrderFormView.stories.tsx
@@ -4,9 +4,11 @@ import {
   withMockApolloDecorator,
   type MockApolloDecoratorArgs,
 } from "@/../.storybook/decorators";
+import { apolloClient } from "@/app/ApolloProvider/apolloClient.js";
 import { getInitialValuesFromSchema } from "@/components/Form/helpers";
+import { QUERIES } from "@/graphql/queries.js";
 import { CoreItemView } from "@/layouts/CoreItemView";
-import { MOCK_WORK_ORDERS } from "@/tests/mockItems/mockWorkOrders.js";
+import { MOCK_WORK_ORDERS, MOCK_MY_CONTACTS_RESPONSE } from "@/tests/mockItems";
 import { WorkOrderForm, type WorkOrderFormProps } from "./WorkOrderForm.jsx";
 import { workOrderFormSchema, type WorkOrderFormValues } from "./schema.js";
 import type { Meta, StoryObj } from "@storybook/react";
@@ -41,6 +43,9 @@ export default meta;
 // STORIES
 
 type Story = StoryObj<typeof meta>;
+
+// Write mocks for queries with "cache-only" fetchPolicy:
+apolloClient.writeQuery({ query: QUERIES.MY_CONTACTS, data: MOCK_MY_CONTACTS_RESPONSE });
 
 export const CreateWorkOrder = {
   args: {
---
+++ src/pages/WorkOrderFormView/elementIDs.ts
@@ -0,0 +1,9 @@
+/**
+ * Element IDs for `WorkOrderFormView` components (src/pages/WorkOrderFormView/).
+ */
+export const woFormViewElementIDs = {
+  detailsGridArea: "wo-form-view__details-grid-area",
+  locationGridArea: "wo-form-view__location-grid-area",
+  entryAndDatesGridArea: "wo-form-view__entry-and-dates-grid-area",
+  priorityAndCategoryContainer: "wo-form-view__priority-and-category-container",
+} as const;
